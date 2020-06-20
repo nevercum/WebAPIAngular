b9478b3cdde91ba188ed5affccc4f7d3ce913bbc
nevercum <121445512+nevercum@users.noreply.github.com>
2020-06-20 06:24:03 +0000

    atualização

--- src/jdk.management.jfr/share/classes/jdk/management/jfr/SettingDescriptorInfo.java
+++ src/jdk.management.jfr/share/classes/jdk/management/jfr/SettingDescriptorInfo.java
@@ -76,4 +76,157 @@ public final class SettingDescriptorInfo {
         this.defaultValue = settingDescriptor.getDefaultValue();
     }
 
-    private SettingDescr
+    private SettingDescriptorInfo(CompositeData cd) {
+        this.name = (String) cd.get("name");
+        this.label = (String) cd.get("label");
+        this.description = (String) cd.get("description");
+        this.typeName = (String) cd.get("typeName");
+        this.defaultValue = (String) cd.get("defaultValue");
+        this.contentType = (String) cd.get("contentType");
+    }
+
+    /**
+    * Returns the human-readable name of the setting associated with this
+     * {@code SettingDescriptorInfo} (for example, {@code "Threshold"}).
+     *
+     * @return the label for this setting, not {@code null}
+     */
+    public String getLabel() {
+        return label;
+    }
+
+    /**
+     * Returns the name of the setting associated with this
+     * {@code SettingDescriptorInfo} (for example, {@code "threshold"}).
+     *
+     * @return the name of this setting, not {@code null}
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the description of the setting associated this
+     * {@code SettingDescriptorInfo} (for example,
+     * {@code "The duration an event must exceed to be be recorded"}).
+     *
+     * @return the description of this setting, not null
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Returns the type name of the setting associated this
+     * {@code SettingDescriptorInfo} (for example,
+     * {@code "jdk.settings.Threshold"}).
+     * <p>
+     * The type can be used to identify what type of setting this is.
+     *
+     * @return the name of this settings type, not {@code null}
+     */
+    public String getTypeName() {
+        return typeName;
+    }
+
+    /**
+     * Returns the content type of the setting associated this
+     * {@code SettingDescriptorInfo} (for example, {@code "jdk.jfr.Timespan"}).
+     * <p>
+     * The content type can be used to determine how the setting should be
+     * rendered in a graphical user interface.
+     *
+     * @return the name of this settings type, not {@code null}
+     */
+    public String getContentType() {
+        return contentType;
+    }
+
+    /**
+     * Returns the default value of the setting associated this
+     * {@code SettingDescriptorInfo} (for example, {@code "20 ms"}).
+     *
+     * @return default value for this setting, not {@code null}
+     *
+     * @see SettingDescriptor#getDefaultValue()
+     */
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    /**
+     * Returns an {@code SettingDescriptorInfo} represented by the specified
+     * {@code CompositeData}
+     * <p>
+     * The supplied {@code CompositeData} must have the following item names and
+     * item types to be valid. <blockquote>
+     * <table class="striped">
+     * <caption>The name and type the specified CompositeData must contain</caption>
+     * <thead>
+     * <tr>
+     * <th scope="col" style="text-align:left">Name</th>
+     * <th scope="col" style="text-align:left">Type</th>
+     * </tr>
+     * </thead>
+     * <tbody>
+     * <tr>
+     * <th scope="row">name</th>
+     * <td>{@code String}</td>
+     * </tr>
+     * <tr>
+     * <th scope="row">label</th>
+     * <td>{@code String}</td>
+     * </tr>
+     * <tr>
+     * <th scope="row">description</th>
+     * <td>{@code String}</td>
+     * </tr>
+     * <tr>
+     * <th scope="row">typeName</th>
+     * <td>{@code String}</td>
+     * </tr>
+     * <tr>
+     * <th scope="row">contentType</th>
+     * <td>{@code String}</td>
+     * </tr>
+     * <tr>
+     * <th scope="row">defaultValue</th>
+     * <td>{@code String}</td>
+     * </tr>
+     * </tbody>
+     * </table>
+     * </blockquote>
+     *
+     * @param cd {@code CompositeData} representing the {@code SettingDescriptorInfo} to
+     *        return
+     *
+     * @throws IllegalArgumentException if {@code cd} does not represent a valid
+     *         {@code EventTypeInfo}
+     *
+     * @return a {@code SettingDescriptorInfo}, or {@code null} if {@code cd} is
+     *         {@code null}
+     */
+    public static SettingDescriptorInfo from(CompositeData cd) {
+        if (cd == null) {
+            return null;
+        }
+        return new SettingDescriptorInfo(cd);
+    }
+
+    /**
+     * Returns a {@code String} description of this {@code SettingDescriptorInfo}.
+     *
+     * @return a string describing this setting, not {@code null}
+     */
+    @Override
+    public String toString() {
+        Stringifier s = new Stringifier();
+        s.add("name", name);
+        s.add("label", label);
+        s.add("description", description);
+        s.add("typeName", typeName);
+        s.add("contentType", contentType);
+        s.add("defaultValue", defaultValue);
+        return s.toString();
+    }
+}
