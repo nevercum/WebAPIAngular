51af6cdce28ab8e57bc7c5cf5d42501a38ce2282
nevercum <121445512+nevercum@users.noreply.github.com>
2020-07-10 11:00:01 +0000

    Commit 6

--- test/langtools/tools/javac/modules/AutomaticModules.java
+++ test/langtools/tools/javac/modules/AutomaticModules.java
@@ -63,4 +63,214 @@ public class AutomaticModules extends ModuleTestBase {
     public void testSimple(Path base) throws Exception {
         Path legacySrc = base.resolve("legacy-src");
         tb.writeJavaFiles(legacySrc,
-                          "package api; import java.awt.event.ActionListener; public abstract class Api implements Acti
+                          "package api; import java.awt.event.ActionListener; public abstract class Api implements ActionListener {}");
+        Path legacyClasses = base.resolve("legacy-classes");
+        Files.createDirectories(legacyClasses);
+
+        String log = new JavacTask(tb)
+                .options()
+                .outdir(legacyClasses)
+                .files(findJavaFiles(legacySrc))
+                .run()
+                .writeAll()
+                .getOutput(Task.OutputKind.DIRECT);
+
+        if (!log.isEmpty()) {
+            throw new Exception("unexpected output: " + log);
+        }
+
+        Path modulePath = base.resolve("module-path");
+
+        Files.createDirectories(modulePath);
+
+        Path jar = modulePath.resolve("test-api-1.0.jar");
+
+        new JarTask(tb, jar)
+          .baseDir(legacyClasses)
+          .files("api/Api.class")
+          .run();
+
+        Path moduleSrc = base.resolve("module-src");
+        Path m1 = moduleSrc.resolve("m1x");
+
+        Path classes = base.resolve("classes");
+
+        Files.createDirectories(classes);
+
+        tb.writeJavaFiles(m1,
+                          "module m1x { requires test.api; requires java.desktop; }",
+                          "package impl; public class Impl { public void e(api.Api api) { api.actionPerformed(null); } }");
+
+        new JavacTask(tb)
+                .options("--module-source-path", moduleSrc.toString(), "--module-path", modulePath.toString())
+                .outdir(classes)
+                .files(findJavaFiles(moduleSrc))
+                .run()
+                .writeAll();
+    }
+
+    @Test
+    public void testUnnamedModule(Path base) throws Exception {
+        Path legacySrc = base.resolve("legacy-src");
+        tb.writeJavaFiles(legacySrc,
+                          "package api; public abstract class Api { public void run(CharSequence str) { } private void run(base.Base base) { } }",
+                          "package base; public interface Base { public void run(); }");
+        Path legacyClasses = base.resolve("legacy-classes");
+        Files.createDirectories(legacyClasses);
+
+        String log = new JavacTask(tb)
+                .options()
+                .outdir(legacyClasses)
+                .files(findJavaFiles(legacySrc))
+                .run()
+                .writeAll()
+                .getOutput(Task.OutputKind.DIRECT);
+
+        if (!log.isEmpty()) {
+            throw new Exception("unexpected output: " + log);
+        }
+
+        Path modulePath = base.resolve("module-path");
+
+        Files.createDirectories(modulePath);
+
+        Path apiJar = modulePath.resolve("test-api-1.0.jar");
+
+        new JarTask(tb, apiJar)
+          .baseDir(legacyClasses)
+          .files("api/Api.class")
+          .run();
+
+        Path baseJar = base.resolve("base.jar");
+
+        new JarTask(tb, baseJar)
+          .baseDir(legacyClasses)
+          .files("base/Base.class")
+          .run();
+
+        Path moduleSrc = base.resolve("module-src");
+        Path m1 = moduleSrc.resolve("m1x");
+
+        Path classes = base.resolve("classes");
+
+        Files.createDirectories(classes);
+
+        tb.writeJavaFiles(m1,
+                          "module m1x { requires test.api; }",
+                          "package impl; public class Impl { public void e(api.Api api) { api.run(\"\"); } }");
+
+        new JavacTask(tb)
+                .options("--module-source-path", moduleSrc.toString(), "--module-path", modulePath.toString(), "--class-path", baseJar.toString())
+                .outdir(classes)
+                .files(findJavaFiles(moduleSrc))
+                .run()
+                .writeAll();
+    }
+
+    @Test
+    public void testModuleInfoFromClassFileDependsOnAutomatic(Path base) throws Exception {
+        Path automaticSrc = base.resolve("automaticSrc");
+        tb.writeJavaFiles(automaticSrc, "package api; public class Api {}");
+        Path automaticClasses = base.resolve("automaticClasses");
+        tb.createDirectories(automaticClasses);
+
+        String automaticLog = new JavacTask(tb)
+                                .outdir(automaticClasses)
+                                .files(findJavaFiles(automaticSrc))
+                                .run()
+                                .writeAll()
+                                .getOutput(Task.OutputKind.DIRECT);
+
+        if (!automaticLog.isEmpty())
+            throw new Exception("expected output not found: " + automaticLog);
+
+        Path modulePath = base.resolve("module-path");
+
+        Files.createDirectories(modulePath);
+
+        Path automaticJar = modulePath.resolve("automatic-1.0.jar");
+
+        new JarTask(tb, automaticJar)
+          .baseDir(automaticClasses)
+          .files("api/Api.class")
+          .run();
+
+        Path depSrc = base.resolve("depSrc");
+        Path depClasses = base.resolve("depClasses");
+
+        Files.createDirectories(depSrc);
+        Files.createDirectories(depClasses);
+
+        tb.writeJavaFiles(depSrc,
+                          "module m1x { requires transitive automatic; }",
+                          "package dep; public class Dep { api.Api api; }");
+
+        new JavacTask(tb)
+                .options("--module-path", modulePath.toString())
+                .outdir(depClasses)
+                .files(findJavaFiles(depSrc))
+                .run()
+                .writeAll();
+
+        Path moduleJar = modulePath.resolve("m1x.jar");
+
+        new JarTask(tb, moduleJar)
+          .baseDir(depClasses)
+          .files("module-info.class", "dep/Dep.class")
+          .run();
+
+        Path testSrc = base.resolve("testSrc");
+        Path testClasses = base.resolve("testClasses");
+
+        Files.createDirectories(testSrc);
+        Files.createDirectories(testClasses);
+
+        tb.writeJavaFiles(testSrc,
+                          "module m2x { requires automatic; }",
+                          "package test; public class Test { }");
+
+        new JavacTask(tb)
+                .options("--module-path", modulePath.toString())
+                .outdir(testClasses)
+                .files(findJavaFiles(testSrc))
+                .run()
+                .writeAll();
+    }
+
+    @Test
+    public void testAutomaticAndNamedModules(Path base) throws Exception {
+        Path modulePath = base.resolve("module-path");
+
+        Files.createDirectories(modulePath);
+
+        for (char c : new char[] {'A', 'B'}) {
+            Path automaticSrc = base.resolve("automaticSrc" + c);
+            tb.writeJavaFiles(automaticSrc, "package api" + c + "; public class Api {}");
+            Path automaticClasses = base.resolve("automaticClasses" + c);
+            tb.createDirectories(automaticClasses);
+
+            String automaticLog = new JavacTask(tb)
+                                    .outdir(automaticClasses)
+                                    .files(findJavaFiles(automaticSrc))
+                                    .run()
+                                    .writeAll()
+                                    .getOutput(Task.OutputKind.DIRECT);
+
+            if (!automaticLog.isEmpty())
+                throw new Exception("expected output not found: " + automaticLog);
+
+            Path automaticJar = modulePath.resolve("automatic" + c + "-1.0.jar");
+
+            new JarTask(tb, automaticJar)
+              .baseDir(automaticClasses)
+              .files("api" + c + "/Api.class")
+              .run();
+        }
+
+        Path moduleSrc = base.resolve("module-src");
+
+        tb.writeJavaFiles(moduleSrc.resolve("m1x"),
+                          "module m1x { requires static automaticA; }",
+                          "package impl; public class Impl { apiA.Api a; apiB.Api b; m2x.M2 m;}");
+
+        tb.writeJavaFile
