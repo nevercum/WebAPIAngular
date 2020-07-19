2efa4b833bd774d8b11d357c054d9c44f6d91b1f
nevercum <121445512+nevercum@users.noreply.github.com>
2020-07-19 15:05:36 +0000

    archive

--- test/jdk/sun/security/pkcs11/KeyGenerator/HmacDefKeySizeTest.java
+++ test/jdk/sun/security/pkcs11/KeyGenerator/HmacDefKeySizeTest.java
@@ -42,4 +42,43 @@ import javax.crypto.SecretKey;
 public class HmacDefKeySizeTest extends PKCS11Test {
 
     /**
-     * Request a KeyGenerator object from PKCS11 provider fo
+     * Request a KeyGenerator object from PKCS11 provider for Hmac algorithm,
+     * and generate the SecretKey.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        main(new HmacDefKeySizeTest(), args);
+    }
+
+    @Override
+    public void main(Provider p) {
+        List<String> algorithms = getSupportedAlgorithms("KeyGenerator",
+                "Hmac", p);
+        boolean success = true;
+
+        for (String alg : algorithms) {
+            System.out.println("Testing " + alg);
+            try {
+                KeyGenerator kg = KeyGenerator.getInstance(alg, p);
+                SecretKey k1 = kg.generateKey();
+                int keysize = k1.getEncoded().length << 3;
+                System.out.println("=> default key size = " + keysize);
+                kg.init(keysize);
+                SecretKey k2 = kg.generateKey();
+                if ((k2.getEncoded().length << 3) != keysize) {
+                    success = false;
+                    System.out.println("keysize check failed");
+                }
+            } catch (Exception e) {
+                System.out.println("Unexpected exception: " + e);
+                e.printStackTrace();
+                success = false;
+            }
+        }
+
+        if (!success) {
+            throw new RuntimeException("One or more tests failed");
+        }
+    }
+}
