d8672b7429e651dc55c4d1f796a56da21e341691
nevercum <121445512+nevercum@users.noreply.github.com>
2020-07-27 15:15:25 +0000

    libceph: remove 'osdtimeout' option

--- test/hotspot/jtreg/compiler/intrinsics/bmi/verifycode/BlsiTestL.java
+++ test/hotspot/jtreg/compiler/intrinsics/bmi/verifycode/BlsiTestL.java
@@ -30,4 +30,27 @@
  *          java.management
  *
  * @build jdk.test.whitebox.WhiteBox
- * @run driver jdk.test.lib.h
+ * @run driver jdk.test.lib.helpers.ClassFileInstaller jdk.test.whitebox.WhiteBox
+ * @run main/othervm -Xbootclasspath/a:. -Xbatch -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI
+ *      -XX:+IgnoreUnrecognizedVMOptions -XX:+UseBMI1Instructions
+ *      compiler.intrinsics.bmi.verifycode.BlsiTestL
+ */
+
+package compiler.intrinsics.bmi.verifycode;
+
+import compiler.intrinsics.bmi.TestBlsiL;
+
+import java.lang.reflect.Method;
+
+public class BlsiTestL extends BlsiTestI {
+
+    protected BlsiTestL(Method method) {
+        super(method);
+        isLongOperation = true;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BmiIntrinsicBase.verifyTestCase(BlsiTestL::new, TestBlsiL.BlsiLExpr.class.getDeclaredMethods());
+        BmiIntrinsicBase.verifyTestCase(BlsiTestL::new, TestBlsiL.BlsiLCommutativeExpr.class.getDeclaredMethods());
+    }
+}
