0abecbd73ebee6704878c6b5d8f06b8b95e5727c
nevercum <121445512+nevercum@users.noreply.github.com>
2020-09-22 18:38:58 +0000

    Update READEME.md

--- test/hotspot/jtreg/vmTestbase/nsk/jvmti/IterateOverReachableObjects/iterreachobj003/iterreachobj003.cpp
+++ test/hotspot/jtreg/vmTestbase/nsk/jvmti/IterateOverReachableObjects/iterreachobj003/iterreachobj003.cpp
@@ -138,4 +138,183 @@ stackReferenceCallback(jvmtiHeapRootKind root_kind,
         nsk_jvmti_setFailStatus();
     }
 
-    if (!NSK_J
+    if (!NSK_JVMTI_VERIFY(st_jvmti->RawMonitorNotify(monitor_ptr))) {
+        nsk_jvmti_setFailStatus();
+    }
+
+    if (!NSK_JVMTI_VERIFY(st_jvmti->RawMonitorNotifyAll(monitor_ptr))) {
+        nsk_jvmti_setFailStatus();
+    }
+
+    if (!NSK_JVMTI_VERIFY(st_jvmti->RawMonitorExit(monitor_ptr))) {
+        nsk_jvmti_setFailStatus();
+        return JVMTI_ITERATION_ABORT;
+    }
+
+    /* Exit second time */
+    if (!NSK_JVMTI_VERIFY(st_jvmti->RawMonitorExit(monitor_ptr))) {
+        nsk_jvmti_setFailStatus();
+        return JVMTI_ITERATION_ABORT;
+    }
+
+    if (!NSK_JVMTI_VERIFY(st_jvmti->DestroyRawMonitor(monitor_ptr))) {
+        nsk_jvmti_setFailStatus();
+    }
+/*
+    NSK_DISPLAY1("stackRefenceCallback: %d\n", objCounter);
+*/
+    return JVMTI_ITERATION_CONTINUE;
+}
+
+
+/* jvmtiObjectReferenceCallback */
+jvmtiIterationControl JNICALL
+objectReferenceCallback(jvmtiObjectReferenceKind reference_kind,
+                        jlong  class_tag,
+                        jlong  size,
+                        jlong* tag_ptr,
+                        jlong  referrer_tag,
+                        jint   referrer_index,
+                        void*  user_data) {
+
+    jrawMonitorID monitor_ptr = NULL;
+
+    *tag_ptr = (jlong)++objCounter;
+
+    if (!NSK_JVMTI_VERIFY(st_jvmti->CreateRawMonitor("objRefMonitor", &monitor_ptr))) {
+        nsk_jvmti_setFailStatus();
+        return JVMTI_ITERATION_ABORT;
+    }
+
+    if (!NSK_JVMTI_VERIFY(st_jvmti->RawMonitorEnter(monitor_ptr))) {
+        nsk_jvmti_setFailStatus();
+        return JVMTI_ITERATION_ABORT;
+    }
+
+    /* Enter second time */
+    if (!NSK_JVMTI_VERIFY(st_jvmti->RawMonitorEnter(monitor_ptr))) {
+        nsk_jvmti_setFailStatus();
+        return JVMTI_ITERATION_ABORT;
+    }
+
+    if (!NSK_JVMTI_VERIFY(st_jvmti->RawMonitorWait(monitor_ptr, (jlong)1))) {
+        nsk_jvmti_setFailStatus();
+    }
+
+    if (!NSK_JVMTI_VERIFY(st_jvmti->RawMonitorNotify(monitor_ptr))) {
+        nsk_jvmti_setFailStatus();
+    }
+
+    if (!NSK_JVMTI_VERIFY(st_jvmti->RawMonitorNotifyAll(monitor_ptr))) {
+        nsk_jvmti_setFailStatus();
+    }
+
+    if (!NSK_JVMTI_VERIFY(st_jvmti->RawMonitorExit(monitor_ptr))) {
+        nsk_jvmti_setFailStatus();
+        return JVMTI_ITERATION_ABORT;
+    }
+
+    /* Exit second time */
+    if (!NSK_JVMTI_VERIFY(st_jvmti->RawMonitorExit(monitor_ptr))) {
+        nsk_jvmti_setFailStatus();
+        return JVMTI_ITERATION_ABORT;
+    }
+
+    if (!NSK_JVMTI_VERIFY(st_jvmti->DestroyRawMonitor(monitor_ptr))) {
+        nsk_jvmti_setFailStatus();
+    }
+
+    NSK_DISPLAY1("objectRefenceCallback: %d\n", objCounter);
+    return JVMTI_ITERATION_ABORT;
+}
+
+/* ============================================================================= */
+
+/** Agent algorithm. */
+static void JNICALL
+agentProc(jvmtiEnv* jvmti, JNIEnv* jni, void* arg) {
+
+    NSK_DISPLAY0("Wait for debugee start\n");
+    if (!NSK_VERIFY(nsk_jvmti_waitForSync(timeout)))
+        return;
+
+    {
+        do {
+            NSK_DISPLAY0("Calling IterateOverReachableObjects\n");
+            {
+                if (!NSK_JVMTI_VERIFY(
+                        jvmti->IterateOverReachableObjects(heapRootCallback,
+                                                           stackReferenceCallback,
+                                                           objectReferenceCallback,
+                                                           &userData))) {
+                    nsk_jvmti_setFailStatus();
+                    break;
+                }
+            }
+
+            if (objCounter == 0) {
+                NSK_COMPLAIN0("IterateOverReachableObjects call had not visited any object\n");
+                nsk_jvmti_setFailStatus();
+                break;
+            } else {
+                NSK_DISPLAY1("Number of objects the IterateOverReachableObjects visited: %d\n", objCounter);
+            }
+
+        } while (0);
+    }
+
+    NSK_DISPLAY0("Let debugee to finish\n");
+    if (!NSK_VERIFY(nsk_jvmti_resumeSync()))
+        return;
+}
+
+/* ============================================================================= */
+
+/* ============================================================================= */
+
+/** Agent library initialization. */
+#ifdef STATIC_BUILD
+JNIEXPORT jint JNICALL Agent_OnLoad_iterreachobj003(JavaVM *jvm, char *options, void *reserved) {
+    return Agent_Initialize(jvm, options, reserved);
+}
+JNIEXPORT jint JNICALL Agent_OnAttach_iterreachobj003(JavaVM *jvm, char *options, void *reserved) {
+    return Agent_Initialize(jvm, options, reserved);
+}
+JNIEXPORT jint JNI_OnLoad_iterreachobj003(JavaVM *jvm, char *options, void *reserved) {
+    return JNI_VERSION_1_8;
+}
+#endif
+jint Agent_Initialize(JavaVM *jvm, char *options, void *reserved) {
+    jvmtiEnv* jvmti = NULL;
+
+    if (!NSK_VERIFY(nsk_jvmti_parseOptions(options)))
+        return JNI_ERR;
+
+    timeout = nsk_jvmti_getWaitTime() * 60 * 1000;
+
+    if (!NSK_VERIFY((jvmti =
+            nsk_jvmti_createJVMTIEnv(jvm, reserved)) != NULL))
+        return JNI_ERR;
+
+    /* save pointer to environment to use it in callbacks */
+    st_jvmti = jvmti;
+
+    {
+        jvmtiCapabilities caps;
+
+        memset(&caps, 0, sizeof(caps));
+        caps.can_tag_objects = 1;
+        if (!NSK_JVMTI_VERIFY(jvmti->AddCapabilities(&caps))) {
+            return JNI_ERR;
+        }
+    }
+
+    if (!NSK_VERIFY(nsk_jvmti_setAgentProc(agentProc, NULL)))
+        return JNI_ERR;
+
+    return JNI_OK;
+}
+
+/* ============================================================================= */
+
+}
