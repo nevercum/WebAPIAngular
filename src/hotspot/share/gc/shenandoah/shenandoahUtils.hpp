eb83a78b9670648755ec4991ef63ab410470f93e
nevercum <121445512+nevercum@users.noreply.github.com>
2020-10-26 20:14:51 +0000

    Prepare Release

--- src/hotspot/share/gc/shenandoah/shenandoahUtils.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoahUtils.hpp
@@ -106,4 +106,121 @@ public:
 
 /*
  * ShenandoahGCPhase tracks Shenandoah specific timing information
- * and emits a 
+ * and emits a corresponding JFR event of a GC phase
+ */
+class ShenandoahGCPhase : public ShenandoahTimingsTracker {
+private:
+  ConcurrentGCTimer* const _timer;
+
+public:
+  ShenandoahGCPhase(ShenandoahPhaseTimings::Phase phase);
+  ~ShenandoahGCPhase();
+};
+
+class ShenandoahGCWorkerPhase : public StackObj {
+private:
+  ShenandoahPhaseTimings* const       _timings;
+  const ShenandoahPhaseTimings::Phase _phase;
+public:
+  ShenandoahGCWorkerPhase(ShenandoahPhaseTimings::Phase phase);
+  ~ShenandoahGCWorkerPhase();
+};
+
+// Aggregates all the things that should happen before/after the pause.
+class ShenandoahGCPauseMark : public StackObj {
+private:
+  ShenandoahHeap* const _heap;
+  const GCIdMark                _gc_id_mark;
+  const SvcGCMarker             _svc_gc_mark;
+  const IsGCActiveMark          _is_gc_active_mark;
+  TraceMemoryManagerStats       _trace_pause;
+
+public:
+  ShenandoahGCPauseMark(uint gc_id, SvcGCMarker::reason_type type);
+};
+
+class ShenandoahSafepoint : public AllStatic {
+public:
+  // Check if Shenandoah GC safepoint is in progress. This is nominally
+  // equivalent to calling SafepointSynchronize::is_at_safepoint(), but
+  // it also checks the Shenandoah specifics, when it can.
+  static inline bool is_at_shenandoah_safepoint() {
+    if (!SafepointSynchronize::is_at_safepoint()) return false;
+
+    Thread* const thr = Thread::current();
+    // Shenandoah GC specific safepoints are scheduled by control thread.
+    // So if we are enter here from control thread, then we are definitely not
+    // at Shenandoah safepoint, but at something else.
+    if (thr == ShenandoahHeap::heap()->control_thread()) return false;
+
+    // This is not VM thread, cannot see what VM thread is doing,
+    // so pretend this is a proper Shenandoah safepoint
+    if (!thr->is_VM_thread()) return true;
+
+    // Otherwise check we are at proper operation type
+    VM_Operation* vm_op = VMThread::vm_operation();
+    if (vm_op == nullptr) return false;
+
+    VM_Operation::VMOp_Type type = vm_op->type();
+    return type == VM_Operation::VMOp_ShenandoahInitMark ||
+           type == VM_Operation::VMOp_ShenandoahFinalMarkStartEvac ||
+           type == VM_Operation::VMOp_ShenandoahInitUpdateRefs ||
+           type == VM_Operation::VMOp_ShenandoahFinalUpdateRefs ||
+           type == VM_Operation::VMOp_ShenandoahFinalRoots ||
+           type == VM_Operation::VMOp_ShenandoahFullGC ||
+           type == VM_Operation::VMOp_ShenandoahDegeneratedGC;
+  }
+};
+
+class ShenandoahWorkerSession : public StackObj {
+protected:
+  ShenandoahWorkerSession(uint worker_id);
+public:
+  static inline uint worker_id() {
+    return WorkerThread::worker_id();
+  }
+};
+
+class ShenandoahConcurrentWorkerSession : public ShenandoahWorkerSession {
+private:
+  EventGCPhaseConcurrent _event;
+
+public:
+  ShenandoahConcurrentWorkerSession(uint worker_id) : ShenandoahWorkerSession(worker_id) { }
+  ~ShenandoahConcurrentWorkerSession();
+};
+
+class ShenandoahParallelWorkerSession : public ShenandoahWorkerSession {
+private:
+  EventGCPhaseParallel _event;
+
+public:
+  ShenandoahParallelWorkerSession(uint worker_id) : ShenandoahWorkerSession(worker_id) { }
+  ~ShenandoahParallelWorkerSession();
+};
+
+class ShenandoahSuspendibleThreadSetJoiner {
+private:
+  SuspendibleThreadSetJoiner _joiner;
+public:
+  ShenandoahSuspendibleThreadSetJoiner(bool active = true) : _joiner(active) {
+    assert(!ShenandoahThreadLocalData::is_evac_allowed(Thread::current()), "STS should be joined before evac scope");
+  }
+  ~ShenandoahSuspendibleThreadSetJoiner() {
+    assert(!ShenandoahThreadLocalData::is_evac_allowed(Thread::current()), "STS should be left after evac scope");
+  }
+};
+
+class ShenandoahSuspendibleThreadSetLeaver {
+private:
+  SuspendibleThreadSetLeaver _leaver;
+public:
+  ShenandoahSuspendibleThreadSetLeaver(bool active = true) : _leaver(active) {
+    assert(!ShenandoahThreadLocalData::is_evac_allowed(Thread::current()), "STS should be left after evac scope");
+  }
+  ~ShenandoahSuspendibleThreadSetLeaver() {
+    assert(!ShenandoahThreadLocalData::is_evac_allowed(Thread::current()), "STS should be joined before evac scope");
+  }
+};
+
+#endif // SHARE_GC_SHENANDOAH_SHENANDOAHUTILS_HPP
