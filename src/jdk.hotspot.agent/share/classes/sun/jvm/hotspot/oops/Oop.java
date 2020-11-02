b370d991ab29e9a95ec3cd29e5095b8da7ac94d0
nevercum <121445512+nevercum@users.noreply.github.com>
2020-11-02 08:47:56 +0000

    Update point_of_sale.js

--- src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/oops/Oop.java
+++ src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/oops/Oop.java
@@ -83,3 +83,134 @@ public class Oop {
       return (Klass)klass.getValue(getHandle());
     }
   }
+
+  public boolean isA(Klass k) {
+    return getKlass().isSubtypeOf(k);
+  }
+
+  // Returns the byte size of this object
+  public long getObjectSize() {
+    Klass k = getKlass();
+    // All other types should be overriding getObjectSize directly
+    return ((InstanceKlass)k).getObjectSize(this);
+  }
+
+  // Type test operations
+  public boolean isInstance()          { return false; }
+  public boolean isInstanceRef()       { return false; }
+  public boolean isArray()             { return false; }
+  public boolean isObjArray()          { return false; }
+  public boolean isTypeArray()         { return false; }
+  public boolean isThread()            { return false; }
+
+  // Align the object size.
+  public static long alignObjectSize(long size) {
+    return VM.getVM().alignUp(size, VM.getVM().getMinObjAlignmentInBytes());
+  }
+
+  // All vm's align longs, so pad out certain offsets.
+  public static long alignObjectOffset(long offset) {
+    return VM.getVM().alignUp(offset, VM.getVM().getBytesPerLong());
+  }
+
+  public boolean equals(Object obj) {
+    if (obj instanceof Oop other) {
+      return getHandle().equals(other.getHandle());
+    }
+    return false;
+ }
+
+  public int hashCode() { return getHandle().hashCode(); }
+
+  /** Identity hash in the target VM */
+  public long identityHash() {
+    Mark mark = getMark();
+    if (mark.isUnlocked() && (!mark.hasNoHash())) {
+      return (int) mark.hash();
+    } else if (mark.isMarked()) {
+      return (int) mark.hash();
+    } else {
+      return slowIdentityHash();
+    }
+  }
+
+  public long slowIdentityHash() {
+    return VM.getVM().getObjectSynchronizer().identityHashValueFor(this);
+  }
+
+  public void iterate(OopVisitor visitor, boolean doVMFields) {
+    visitor.setObj(this);
+    visitor.prologue();
+    iterateFields(visitor, doVMFields);
+    visitor.epilogue();
+  }
+
+  void iterateFields(OopVisitor visitor, boolean doVMFields) {
+    if (doVMFields) {
+      visitor.doCInt(mark, true);
+      if (VM.getVM().isCompressedKlassPointersEnabled()) {
+        visitor.doMetadata(compressedKlass, true);
+      } else {
+        visitor.doMetadata(klass, true);
+      }
+    }
+  }
+
+  public void print()      { printOn(System.out); }
+  public void printValue() { printValueOn(System.out); }
+  public void printRaw()   { printRawOn(System.out); }
+
+  public static void printOopValueOn(Oop obj, PrintStream tty) {
+    if (obj == null) {
+      tty.print("null");
+    } else {
+      obj.printValueOn(tty);
+      tty.print(" @ " + VM.getVM().getUniverse().heap().oopAddressDescription(obj.getHandle()));
+    }
+  }
+
+  public static void printOopAddressOn(Oop obj, PrintStream tty) {
+    if (obj == null) {
+      tty.print("null");
+    } else {
+      tty.print(obj.getHandle().toString());
+    }
+  }
+
+  public void printOn(PrintStream tty) {
+    OopPrinter printer = new OopPrinter(tty);
+    iterate(printer, true);
+  }
+
+  public void printValueOn(PrintStream tty) {
+    try {
+      tty.print("Oop for " + getKlass().getName().asString());
+    } catch (java.lang.NullPointerException e) {
+      tty.print("Oop");
+    }
+  }
+
+  public void printRawOn(PrintStream tty) {
+    tty.print("Dumping raw memory for ");
+    printValueOn(tty);
+    tty.println();
+    long size = getObjectSize() * 4;
+    for (long i = 0; i < size; i += 4) {
+      long memVal = getHandle().getCIntegerAt(i, 4, true);
+      tty.println(Long.toHexString(memVal));
+    }
+  }
+
+  public boolean verify() { return true;}
+
+  public static Klass getKlassForOopHandle(OopHandle handle) {
+    if (handle == null) {
+      return null;
+    }
+    if (VM.getVM().isCompressedKlassPointersEnabled()) {
+      return (Klass)Metadata.instantiateWrapperFor(handle.getCompKlassAddressAt(compressedKlass.getOffset()));
+    } else {
+      return (Klass)Metadata.instantiateWrapperFor(handle.getAddressAt(klass.getOffset()));
+    }
+  }
+};
