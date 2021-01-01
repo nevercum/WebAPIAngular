952c35ebb2519b1eddcaa65b3c9b656c94e7b929
nevercum <121445512+nevercum@users.noreply.github.com>
2021-01-01 10:38:42 +0000

    Rejected commit: Default

--- test/hotspot/jtreg/vmTestbase/nsk/jdi/LocalVariable/typeName/typename001/TestDescription.java
+++ test/hotspot/jtreg/vmTestbase/nsk/jdi/LocalVariable/typeName/typename001/TestDescription.java
@@ -41,4 +41,43 @@
  *     hence, AbsentInformationException is not expected to happen.
  *     The test works as follows:
  *     The debugger program - nsk.jdi.LocalVariable.typeName.typename001;
- *     the debuggee program - nsk.jdi.LocalVariable.typeName.typename001
+ *     the debuggee program - nsk.jdi.LocalVariable.typeName.typename001a.
+ *     Using nsk.jdi.share classes,
+ *     the debugger gets the debuggee running on another JavaVM,
+ *     creates the object debuggee.VM,
+ *     establishes a pipe with the debuggee program, and then
+ *     send to the programm commands, to which the debuggee replies
+ *     via the pipe. Upon getting reply,
+ *     the debugger calls corresponding debuggee.VM methods to get
+ *     needed data and compares the data got to the data expected.
+ *     In case of error the test produces the return value 97 and
+ *     a corresponding error message(s).
+ *     Otherwise, the test is passed and produces
+ *     the return value 95 and no message.
+ * COMMENTS:
+ *     This test is option depended,
+ *     that is its .cfg file contains the option
+ *         JAVAC_OPTS=-g
+ *     because at the date of preparing the test
+ *     javac prepared full information for the test only
+ *     been invoked with the option.
+ *
+ * @library /vmTestbase
+ *          /test/lib
+ * @build nsk.jdi.LocalVariable.typeName.typename001
+ *        nsk.jdi.LocalVariable.typeName.typename001a
+ *
+ * @comment make sure typename001a is compiled with full debug info
+ * @clean nsk.jdi.LocalVariable.typeName.typename001a
+ * @compile -g:lines,source,vars ../typename001a.java
+ *
+ * @run main/othervm
+ *      nsk.jdi.LocalVariable.typeName.typename001
+ *      -verbose
+ *      -arch=${os.family}-${os.simpleArch}
+ *      -waittime=5
+ *      -debugee.vmkind=java
+ *      -transport.address=dynamic
+ *      -debugee.vmkeys="${test.vm.opts} ${test.java.opts}"
+ */
+
