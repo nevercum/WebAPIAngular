b2d051a53c0d78a32c1f202bb3f7a7ef4087a2a6
nevercum <121445512+nevercum@users.noreply.github.com>
2021-01-04 13:10:17 +0000

    Modify layout

--- test/jdk/java/util/concurrent/locks/Lock/OOMEInAQS.java
+++ test/jdk/java/util/concurrent/locks/Lock/OOMEInAQS.java
@@ -49,4 +49,94 @@ public class OOMEInAQS extends Thread {
     static final Condition condition = mainLock.newCondition();
     static final CountDownLatch started = new CountDownLatch(1);
     static final CountDownLatch filled = new CountDownLatch(1);
-    static final CountDownLatch canFill = new CountDownLatch(NTHRE
+    static final CountDownLatch canFill = new CountDownLatch(NTHREADS);
+    static volatile Object data;
+    static volatile Throwable exception;
+    static int turn;
+
+    /**
+     * For each of NTHREADS threads, REPS times: Take turns
+     * executing. Introduce OOM using fillHeap during runs. In
+     * addition to testing AQS, the CountDownLatches ensure that
+     * methods execute at least once before OutOfMemory occurs, to
+     * avoid uncontrollable impact of OOME during class-loading.
+     */
+    public static void main(String[] args) throws Throwable {
+        OOMEInAQS[] threads = new OOMEInAQS[NTHREADS];
+        for (int i = 0; i < NTHREADS; ++i)
+            (threads[i] = new OOMEInAQS(i)).start();
+        started.countDown();
+        canFill.await();
+        long t0 = System.nanoTime();
+        data = fillHeap();
+        filled.countDown();
+        long t1 = System.nanoTime();
+        for (int i = 0; i < NTHREADS; ++i)
+            threads[i].join();
+        data = null;  // free heap before reporting and terminating
+        System.gc();
+        Throwable ex = exception;
+        if (ex != null)
+            throw ex;
+        System.out.println(
+            "fillHeap time: " + (t1 - t0) / 1000_000 +
+            " millis, whole test time: " + (System.nanoTime() - t0) / 1000_000 +
+            " millis"
+        );
+    }
+
+    final int tid;
+    OOMEInAQS(int tid) {
+        this.tid = tid;
+    }
+
+    @Override
+    public void run() {
+        int id = tid, nextId = (id + 1) % NTHREADS;
+        final ReentrantLock lock = mainLock;
+        final Condition cond = condition;
+        try {
+            started.await();
+            for (int i = 0; i < NREPS; i++) {
+                lock.lock();
+                try {
+                    while (turn != id)
+                        cond.await();
+                    turn = nextId;
+                    cond.signalAll();
+                } finally {
+                    lock.unlock();
+                }
+                if (i == 2) {  // Subsequent AQS methods encounter OOME
+                    canFill.countDown();
+                    filled.await();
+                }
+            }
+            data = null;
+            System.gc(); // avoid getting stuck while exiting
+        } catch (Throwable ex) {
+            data = null;
+            System.gc(); // avoid nested OOME
+            exception = ex;
+        }
+    }
+
+    static Object[] fillHeap() {
+        Object[] first = null, last = null;
+        int size = 1 << 20;
+        while (size > 0) {
+            try {
+                Object[] array = new Object[size];
+                if (first == null) {
+                    first = array;
+                } else {
+                    last[0] = array;
+                }
+                last = array;
+            } catch (OutOfMemoryError oome) {
+                size = size >>> 1;
+            }
+        }
+        return first;
+    }
+}
