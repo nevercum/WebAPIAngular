9d6e1da49d81436fd85daa7ff8dbccce969331f1
nevercum <121445512+nevercum@users.noreply.github.com>
2021-01-06 10:01:12 +0000

    remove duplicate entry

--- src/java.desktop/macosx/classes/com/apple/laf/ScreenMenuBar.java
+++ src/java.desktop/macosx/classes/com/apple/laf/ScreenMenuBar.java
@@ -98,4 +98,178 @@ public class ScreenMenuBar extends MenuBar
             // We don't listen to events when the menu bar is not parented.
             // Remove all the component listeners.
             final Enumeration<JMenu> e = fSubmenus.keys();
-            whi
+            while (e.hasMoreElements()) {
+                final JMenu m = e.nextElement();
+                m.removeComponentListener(this);
+            }
+        }
+
+        super.removeNotify();
+    }
+
+    /**
+     * Invoked when a component has been added to the container.
+     */
+    public void componentAdded(final ContainerEvent e) {
+        final Component child = e.getChild();
+        if (!(child instanceof JMenu)) return;
+            addSubmenu((JMenu)child);
+     }
+
+    /**
+     * Invoked when a component has been removed from the container.
+     */
+    public void componentRemoved(final ContainerEvent e) {
+          final Component child = e.getChild();
+          if (!(child instanceof JMenu)) return;
+            removeSubmenu((JMenu)child);
+        }
+
+    /**
+        * Invoked when the component's size changes.
+     */
+    public void componentResized(final ComponentEvent e)  {}
+
+    /**
+        * Invoked when the component's position changes.
+     */
+    public void componentMoved(final ComponentEvent e)  {}
+
+    /**
+        * Invoked when the component has been made visible.
+     * See componentHidden - we should still have a MenuItem
+     * it just isn't inserted
+     */
+    public void componentShown(final ComponentEvent e) {
+        final Object source = e.getSource();
+        if (!(source instanceof JMenuItem)) return;
+        setChildVisible((JMenuItem)source, true);
+    }
+
+    /**
+        * Invoked when the component has been made invisible.
+     * MenuComponent.setVisible does nothing,
+     * so we remove the ScreenMenuItem from the ScreenMenu
+     * but leave it in fItems
+     */
+    public void componentHidden(final ComponentEvent e)  {
+        final Object source = e.getSource();
+        if (!(source instanceof JMenuItem)) return;
+        setChildVisible((JMenuItem)source, false);
+    }
+
+    /*
+     * MenuComponent.setVisible does nothing,
+     * so we just add or remove the child from the ScreenMenuBar
+     * but leave it in the list
+     */
+    public void setChildVisible(final JMenuItem child, final boolean b) {
+        if (child instanceof JMenu) {
+            if (b) {
+                addSubmenu((JMenu)child);
+            } else {
+                final ScreenMenu sm = fSubmenus.get(child);
+                if (sm != null)
+                    remove(sm);
+            }
+        }
+    }
+
+    public void removeAll() {
+        synchronized (getTreeLock()) {
+            final int nitems = getMenuCount();
+            for (int i = nitems-1 ; i >= 0 ; i--) {
+                remove(i);
+            }
+        }
+    }
+
+    public void setIcon(final Icon i) {}
+    public void setLabel(final String s) {}
+
+    public void setEnabled(final boolean b) {
+        final int count = fSwingBar.getMenuCount();
+        for (int i = 0; i < count; i++) {
+            fSwingBar.getMenu(i).setEnabled(b);
+        }
+    }
+
+    public void setAccelerator(final KeyStroke ks) {}
+    public void setToolTipText(final String tooltip) {}
+
+    // only check and radio items can be indeterminate
+    public void setIndeterminate(boolean indeterminate) { }
+
+    ScreenMenu addSubmenu(final JMenu m) {
+        ScreenMenu sm = fSubmenus.get(m);
+
+        if (sm == null) {
+            sm = new ScreenMenu(m);
+            m.addComponentListener(this);
+            fSubmenus.put(m, sm);
+        }
+
+        sm.setEnabled(m.isEnabled());
+
+        // MenuComponents don't support setVisible, so we just don't add it to the menubar
+        if (m.isVisible() && sm.getParent() == null) {
+            int newIndex = 0, currVisibleIndex = 0;
+            JMenu menu = null;
+            final int menuCount = fSwingBar.getMenuCount();
+            for (int i = 0; i < menuCount; i++) {
+                menu = fSwingBar.getMenu(i);
+                if (menu == m) {
+                    newIndex = currVisibleIndex;
+                    break;
+                }
+                if (menu != null && menu.isVisible()) {
+                    currVisibleIndex++;
+                }
+            }
+            add(sm, newIndex);
+        }
+
+        return sm;
+    }
+
+    /**
+     * Remove the screen menu associated with the specifiec menu.  This
+     * also removes any associated component listener on the screen menu
+     * and removes the key/value (menu/screen menu) from the fSubmenus cache.
+     *
+     * @param menu The swing menu we want to remove the screen menu for.
+     */
+    private void removeSubmenu(final JMenu menu) {
+        final ScreenMenu screenMenu = fSubmenus.get(menu);
+        if (screenMenu == null) return;
+
+            menu.removeComponentListener(this);
+            remove(screenMenu);
+            fSubmenus.remove(menu);
+    }
+
+    public Menu add(final Menu m, final int index) {
+        synchronized (getTreeLock()) {
+            if (m.getParent() != null) {
+                m.getParent().remove(m);
+            }
+
+            final Vector<Menu> menus = getMenuBarAccessor().getMenus(this);
+            menus.insertElementAt(m, index);
+            final MenuComponentAccessor acc = getMenuComponentAccessor();
+            acc.setParent(m, this);
+
+            final CMenuBar peer = acc.getPeer(this);
+            if (peer == null) return m;
+
+            peer.setNextInsertionIndex(index);
+            final CMenuBar mPeer = acc.getPeer(m);
+            if (mPeer == null) {
+                m.addNotify();
+            }
+
+            peer.setNextInsertionIndex(-1);
+            return m;
+        }
+    }
+}
