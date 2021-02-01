a7a13b22bc527c324d4b9a7fda28d31c1f70214e
nevercum <121445512+nevercum@users.noreply.github.com>
2021-02-01 19:22:47 +0000

    Added readme files

--- src/java.desktop/share/classes/javax/swing/tree/DefaultTreeModel.java
+++ src/java.desktop/share/classes/javax/swing/tree/DefaultTreeModel.java
@@ -5,4 +5,212 @@
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
  * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject t
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package javax.swing.tree;
+
+import java.beans.ConstructorProperties;
+import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.EventListener;
+import java.util.Vector;
+
+import javax.swing.event.EventListenerList;
+import javax.swing.event.TreeModelEvent;
+import javax.swing.event.TreeModelListener;
+
+/**
+ * A simple tree data model that uses TreeNodes.
+ * For further information and examples that use DefaultTreeModel,
+ * see <a href="https://docs.oracle.com/javase/tutorial/uiswing/components/tree.html">How to Use Trees</a>
+ * in <em>The Java Tutorial.</em>
+ * <p>
+ * <strong>Warning:</strong>
+ * Serialized objects of this class will not be compatible with
+ * future Swing releases. The current serialization support is
+ * appropriate for short term storage or RMI between applications running
+ * the same version of Swing.  As of 1.4, support for long term storage
+ * of all JavaBeans
+ * has been added to the <code>java.beans</code> package.
+ * Please see {@link java.beans.XMLEncoder}.
+ *
+ * @author Rob Davis
+ * @author Ray Ryan
+ * @author Scott Violet
+ */
+@SuppressWarnings("serial") // Same-version serialization only
+public class DefaultTreeModel implements Serializable, TreeModel {
+    /** Root of the tree. */
+    protected TreeNode root;
+    /** Listeners. */
+    protected EventListenerList listenerList = new EventListenerList();
+    /**
+      * Determines how the <code>isLeaf</code> method figures
+      * out if a node is a leaf node. If true, a node is a leaf
+      * node if it does not allow children. (If it allows
+      * children, it is not a leaf node, even if no children
+      * are present.) That lets you distinguish between <i>folder</i>
+      * nodes and <i>file</i> nodes in a file system, for example.
+      * <p>
+      * If this value is false, then any node which has no
+      * children is a leaf node, and any node may acquire
+      * children.
+      *
+      * @see TreeNode#getAllowsChildren
+      * @see TreeModel#isLeaf
+      * @see #setAsksAllowsChildren
+      */
+    protected boolean asksAllowsChildren;
+
+
+    /**
+      * Creates a tree in which any node can have children.
+      *
+      * @param root a TreeNode object that is the root of the tree
+      * @see #DefaultTreeModel(TreeNode, boolean)
+      */
+     @ConstructorProperties({"root"})
+     public DefaultTreeModel(TreeNode root) {
+        this(root, false);
+    }
+
+    /**
+      * Creates a tree specifying whether any node can have children,
+      * or whether only certain nodes can have children.
+      *
+      * @param root a TreeNode object that is the root of the tree
+      * @param asksAllowsChildren a boolean, false if any node can
+      *        have children, true if each node is asked to see if
+      *        it can have children
+      * @see #asksAllowsChildren
+      */
+    public DefaultTreeModel(TreeNode root, boolean asksAllowsChildren) {
+        super();
+        this.root = root;
+        this.asksAllowsChildren = asksAllowsChildren;
+    }
+
+    /**
+      * Sets whether or not to test leafness by asking getAllowsChildren()
+      * or isLeaf() to the TreeNodes.  If newvalue is true, getAllowsChildren()
+      * is messaged, otherwise isLeaf() is messaged.
+      *
+      * @param newValue if true, getAllowsChildren() is messaged, otherwise
+      *                 isLeaf() is messaged
+      */
+    public void setAsksAllowsChildren(boolean newValue) {
+        asksAllowsChildren = newValue;
+    }
+
+    /**
+      * Tells how leaf nodes are determined.
+      *
+      * @return true if only nodes which do not allow children are
+      *         leaf nodes, false if nodes which have no children
+      *         (even if allowed) are leaf nodes
+      * @see #asksAllowsChildren
+      */
+    public boolean asksAllowsChildren() {
+        return asksAllowsChildren;
+    }
+
+    /**
+     * Sets the root to <code>root</code>. A null <code>root</code> implies
+     * the tree is to display nothing, and is legal.
+     *
+     * @param root new value of tree root
+     */
+    public void setRoot(TreeNode root) {
+        Object oldRoot = this.root;
+        this.root = root;
+        if (root == null && oldRoot != null) {
+            fireTreeStructureChanged(this, null);
+        }
+        else {
+            nodeStructureChanged(root);
+        }
+    }
+
+    /**
+     * Returns the root of the tree.  Returns null only if the tree has
+     * no nodes.
+     *
+     * @return  the root of the tree
+     */
+    public Object getRoot() {
+        return root;
+    }
+
+    /**
+     * Returns the index of child in parent.
+     * If either the parent or child is <code>null</code>, returns -1.
+     * @param parent a note in the tree, obtained from this data source
+     * @param child the node we are interested in
+     * @return the index of the child in the parent, or -1
+     *    if either the parent or the child is <code>null</code>
+     */
+    public int getIndexOfChild(Object parent, Object child) {
+        if(parent == null || child == null)
+            return -1;
+        return ((TreeNode)parent).getIndex((TreeNode)child);
+    }
+
+    /**
+     * Returns the child of <I>parent</I> at index <I>index</I> in the parent's
+     * child array.  <I>parent</I> must be a node previously obtained from
+     * this data source. This should not return null if <i>index</i>
+     * is a valid index for <i>parent</i> (that is <i>index</i> &gt;= 0 &amp;&amp;
+     * <i>index</i> &lt; getChildCount(<i>parent</i>)).
+     *
+     * @param   parent  a node in the tree, obtained from this data source
+     * @return  the child of <I>parent</I> at index <I>index</I>
+     */
+    public Object getChild(Object parent, int index) {
+        return ((TreeNode)parent).getChildAt(index);
+    }
+
+    /**
+     * Returns the number of children of <I>parent</I>.  Returns 0 if the node
+     * is a leaf or if it has no children.  <I>parent</I> must be a node
+     * previously obtained from this data source.
+     *
+     * @param   parent  a node in the tree, obtained from this data source
+     * @return  the number of children of the node <I>parent</I>
+     */
+    public int getChildCount(Object parent) {
+        return ((TreeNode)parent).getChildCount();
+    }
+
+    /**
+     * Returns whether the specified node is a leaf node.
+     * The way the test is performed depends on the
+     * <code>asksAllowsChildren</code> setting.
+     *
+     * @param node the node to check
+     * @return true if the node is a leaf node
+     *
+     * @see #asksAllowsChildren
+     * @see TreeModel#isLeaf
+     */
+    public boolean isLeaf(Object node) {
+    
