d1af3d2d87e0eb0c2150d0621de2bf8f711c3f7d
nevercum <121445512+nevercum@users.noreply.github.com>
2021-02-15 20:35:19 +0000

    readme up

--- test/hotspot/jtreg/runtime/cds/appcds/test-classes/JvmtiApp.java
+++ test/hotspot/jtreg/runtime/cds/appcds/test-classes/JvmtiApp.java
@@ -14,4 +14,99 @@
  *
  * You should have received a copy of the GNU General Public License version
  * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+import jdk.test.whitebox.WhiteBox;
+
+public class JvmtiApp {
+    static Class forname(String cn) {
+        try {
+            return Class.forName(cn);
+        } catch (Throwable t) {
+            return null;
+        }
+    }
+
+    static void failed(String msg) {
+        System.out.println("TEST FAILED: " + msg);
+        System.exit(1);
+    }
+
+    // See ../JvmtiAddPath.java for how the classpaths are configured.
+    public static void main(String args[]) {
+        String cn = "Hello";
+        if (args.length >= 3) {
+            cn = args[args.length - 1];
+        }
+
+        if (args[0].equals("noadd")) {
+            if (forname(cn) != null) {
+                failed(cn + " class was loaded unexpectedly");
+            }
+            // We use -verbose:class to verify that Extra.class IS loaded by AppCDS if
+            // the boot classpath HAS NOT been appended.
+            ExtraClass.doit();
+            System.exit(0);
+        }
+
+        WhiteBox wb = WhiteBox.getWhiteBox();
+
+        if (args[0].equals("bootonly")) {
+            wb.addToBootstrapClassLoaderSearch(args[1]);
+            Class cls = forname(cn);
+            if (cls == null) {
+                failed("Cannot find " + cn + " class");
+            }
+            if (cls.getClassLoader() != null) {
+                failed("Hello class not loaded by boot classloader");
+            }
+        } else if (args[0].equals("apponly")) {
+            wb.addToSystemClassLoaderSearch(args[1]);
+            Class cls = forname(cn);
+            if (cls == null) {
+                failed("Cannot find " + cn + " class");
+            }
+            if (cls.getClassLoader() != JvmtiApp.class.getClassLoader()) {
+                failed(cn + " class not loaded by app classloader");
+            }
+        } else if (args[0].equals("noadd-appcds")) {
+            cn = (args.length == 1) ? "Hello" : args[1];
+            Class cls = forname(cn);
+            if (cls == null) {
+                failed("Cannot find " + cn + " class");
+            }
+            if (cls.getClassLoader() != JvmtiApp.class.getClassLoader()) {
+                failed(cn + " class not loaded by app classloader");
+            }
+        } else if (args[0].equals("appandboot")) {
+            wb.addToBootstrapClassLoaderSearch(args[1]);
+            wb.addToSystemClassLoaderSearch(args[2]);
+            cn = (args.length == 3) ? "Hello" : args[3];
+            Class cls = forname(cn);
+            if (cls == null) {
+                failed("Cannot find " + cn + " class");
+            }
+            if (cls.getClassLoader() != null) {
+                failed(cn + " class not loaded by boot classloader");
+            }
+        } else {
+            failed("unknown option " + args[0]);
+        }
+
+        // We use -verbose:class to verify that Extra.class IS NOT loaded by AppCDS if
+        // the boot classpath HAS been appended.
+        ExtraClass.doit();
+
+        System.out.println("Test passed: " + args[0]);
+    }
+}
+
+class ExtraClass {
+    static void doit() {}
+}
