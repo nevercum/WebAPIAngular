a95ad6a23e638b345c0a7c5f11e814ed5a0a53ff
nevercum <121445512+nevercum@users.noreply.github.com>
2021-02-21 15:09:54 +0000

    Update Command.php

--- src/java.naming/share/classes/javax/naming/InsufficientResourcesException.java
+++ src/java.naming/share/classes/javax/naming/InsufficientResourcesException.java
@@ -40,4 +40,33 @@ package javax.naming;
   * Synchronization and serialization issues that apply to NamingException
   * apply directly here.
   *
- 
+  * @author Rosanna Lee
+  * @author Scott Seligman
+  * @since 1.3
+  */
+
+public class InsufficientResourcesException extends NamingException {
+    /**
+     * Constructs a new instance of InsufficientResourcesException using an
+     * explanation. All other fields default to null.
+     *
+     * @param   explanation     Possibly null additional detail about this exception.
+     * @see java.lang.Throwable#getMessage
+     */
+    public InsufficientResourcesException(String explanation) {
+        super(explanation);
+    }
+
+    /**
+      * Constructs a new instance of InsufficientResourcesException with
+      * all name resolution fields and explanation initialized to null.
+      */
+    public InsufficientResourcesException() {
+        super();
+    }
+
+    /**
+     * Use serialVersionUID from JNDI 1.1.1 for interoperability
+     */
+    private static final long serialVersionUID = 6227672693037844532L;
+}
