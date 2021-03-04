835250c1400bfe14ef12074d722af37baa0804f4
nevercum <121445512+nevercum@users.noreply.github.com>
2021-03-04 11:33:57 +0000

    tests: fix

--- test/langtools/jdk/javadoc/doclet/testGroupOption/TestGroupOption.java
+++ test/langtools/jdk/javadoc/doclet/testGroupOption/TestGroupOption.java
@@ -60,4 +60,32 @@ public class TestGroupOption extends JavadocTester {
     // Make sure the "Other packages" section is printed and the header for empty section is not.
     // Make sure that the headers of group that is defined using patterns are printed.
     public void test2() {
-  
+        javadoc("-d", "out-2",
+                "-sourcepath", testSrc,
+                "-group", "Group pkg*", "pkg*",
+                "-group", "Group abc*", "abc*",
+                "-group", "Empty group", "qwerty*",
+                "-group", "Group a*", "a*",
+                "pkg1", "pkg2", "pkg3", "abc1",  "abc2", "abc3", "other", testSrc("InUnnamedPackage.java"));
+        checkExit(Exit.OK);
+
+        checkOutput("index.html", true, "Group pkg*", "Group abc*", "Other Packages");
+        checkOutput("index.html", false, "Empty group", "Group a*");
+    }
+
+    @Test
+    public void test3() {
+        // Make sure the warning is printed when -group is not used correctly.
+        javadoc("-d", "out-3",
+                "-sourcepath", testSrc,
+                "-group", "Package One", "pkg1",
+                "-group", "Package One", "pkg2",
+                "-group", "Package One", "pkg3",
+                "pkg1", "pkg2", "pkg3");
+        checkExit(Exit.OK);
+
+        checkOutput(Output.OUT, true,
+                "-group");
+
+    }
+}
