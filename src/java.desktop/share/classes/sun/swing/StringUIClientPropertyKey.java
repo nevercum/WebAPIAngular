7467ba080599fe9d68cdb78cdc8b5ec7b0b3b235
nevercum <121445512+nevercum@users.noreply.github.com>
2021-03-09 10:52:09 +0000

    Added CMakeLists.txt

--- src/java.desktop/share/classes/sun/swing/StringUIClientPropertyKey.java
+++ src/java.desktop/share/classes/sun/swing/StringUIClientPropertyKey.java
@@ -36,4 +36,11 @@ public class StringUIClientPropertyKey implements UIClientPropertyKey {
 
     private final String key;
 
-   
+    public StringUIClientPropertyKey(String key) {
+        this.key = key;
+    }
+
+    public String toString() {
+        return key;
+    }
+}
---
+++ src/jdk.crypto.cryptoki/share/native/libj2pkcs11/p11_keymgmt.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2003, 2022, Oracle and/or its affiliates. All rights reserved.
+ */
+
+/* Copyright  (c) 2002 Graz University of Technology. All rights reserved.
+ *
+ * Redistribution and use in  source and binary forms, with or without
+ * modification, are permitted  provided that the following conditions are met:
+ *
+ * 1. Redistributions of  source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in  binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if any, must
+ *    include the following acknowledgment:
+ *
+ *    "This product
