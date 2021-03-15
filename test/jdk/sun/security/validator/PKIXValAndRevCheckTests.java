98391f440bf4b2dcbd0e03b0a4ba719d8a72c165
nevercum <121445512+nevercum@users.noreply.github.com>
2021-03-15 11:12:23 +0000

    Fixed validation

--- test/jdk/sun/security/validator/PKIXValAndRevCheckTests.java
+++ test/jdk/sun/security/validator/PKIXValAndRevCheckTests.java
@@ -273,4 +273,133 @@ public class PKIXValAndRevCheckTests {
         "L3YuMDBhxOcu4p17g7zAo5IxUb/Vzh9tAVQCAzEzHiAcpOjUCWHnmk0Rl02sGTfP\n" +
         "3Gl7ULqrERs/6tr2vyPgORyL7bvr7o3VZLLtm2mnxjNEJfBnc4/DHSxm/pBhY4vx\n" +
         "V3/tjy/FH6Z5lIpCze9vJRfW83ANOOiPAeN0q4hSXmDpSz3xP8ozYOXnHHTDwwcO\n" +
-        "4SkD1W136XJlkSZv7xpQkd/EXe3HmBd/3LX/9CfA2m+6vfCE6R/
+        "4SkD1W136XJlkSZv7xpQkd/EXe3HmBd/3LX/9CfA2m+6vfCE6R/auTRQ/qNjSWaB\n" +
+        "auxwT3RkyCe95t1j8y0a1PiTw+8bPcIJrrkauoVP5T0neSCUpCf3Ml5JmtUcATuC\n" +
+        "5q1NmlgxoTcCAwEAAaOBnjCBmzAdBgNVHQ4EFgQUmtpA0z5braXCrNxIRzpWM1cB\n" +
+        "pJowHwYDVR0jBBgwFoAU8FiCLlBAJ3gFyXrnd4qGpJk4N9swDwYDVR0TAQH/BAUw\n" +
+        "AwEB/zAOBgNVHQ8BAf8EBAMCAYYwOAYIKwYBBQUHAQEELDAqMCgGCCsGAQUFBzAB\n" +
+        "hhxodHRwOi8vamlhbi5vc3RhcGxlLm9yZzo3MTAwMA0GCSqGSIb3DQEBCwUAA4IC\n" +
+        "AQA0aIIpyhYoCzis9TSTfOsOunj96+A/+UKvPhvQ4mrJk4S0H2lWAb7mkfZC9w/V\n" +
+        "a9RBF0RpgsTEx3rVtEkP0rDHkWwX/CIXJODDngAvXDMrL7TX46cMWgIesxeW1+KY\n" +
+        "4994CaYzyzQGa7tPa0hdBiKPQsT4fawTpJrba5zw1+6y0ffetovz+Ch8h5JxjO4G\n" +
+        "NFPPHXboJADYhSIgHwURhc9wKjIjIt1T+Ukwj9JNGm/365Hr+gv6qt7YeIEPwaIP\n" +
+        "+WGqFr8xgLO0KEO/q9ASEWYRu9VkCg+GMdWuN0Zxt7RTyxc55NKHXofLK/wiNx//\n" +
+        "m3ssiS3dNgKbo2aAHzB803Kv9u5jtWbwOakreMLG+4LMJyGMF2yWQQUaIsRhQ+1T\n" +
+        "9DRF0RJY6RLhiCMtpZB2+cDVdzhDJgJqCsMdRl1l0X0ER6p9X9v1y2CJw4NLvbvh\n" +
+        "ulF93nZ+q8D/r5SF9gseKgv8l+M4O2MTFwsK6Jfdf+8LoJFBj45lOqpivk7bsGTU\n" +
+        "BtsLQgeBSV2tGGcg3e1PxR+51mbRvQYM6S4rfdIQuPpuVeBP+yOrJb8MaZBO8tDY\n" +
+        "7rY8v6nLopeopqzkf2qbsE9XnAJso5tVJqSjyy82JxOgLUijk53Rmtojcjw67HhS\n" +
+        "8u01Bf34P3b2eOjpqLkl97qQBUZ76V2F8EkEUNkvsBJ26Q==";
+
+    // OCSP Response Status: successful (0x0)
+    // Response Type: Basic OCSP Response
+    // Version: 1 (0x0)
+    // Responder Id: O = TestPKI, CN = CA1 Intermediate
+    // Produced At: Aug 16 05:03:27 2019 GMT
+    // Responses:
+    // Certificate ID:
+    //   Hash Algorithm: sha1
+    //   Issuer Name Hash: FE48D59BAF624773549AE209AA14FD20DCE6B8F4
+    //   Issuer Key Hash: 9ADA40D33E5BADA5C2ACDC48473A56335701A49A
+    //   Serial Number: 01000016
+    // Cert Status: revoked
+    // Revocation Time: Aug 16 04:59:31 2019 GMT
+    // Revocation Reason: keyCompromise (0x1)
+    // This Update: Aug 16 05:03:27 2019 GMT
+    // Next Update: Aug 17 05:03:27 2019 GMT
+    static final String BAD_GUY_OCSP_PEM =
+        "MIIGlAoBAKCCBo0wggaJBgkrBgEFBQcwAQEEggZ6MIIGdjCB5qEvMC0xEDAOBgNV\n" +
+        "BAoMB1Rlc3RQS0kxGTAXBgNVBAMMEENBMSBJbnRlcm1lZGlhdGUYDzIwMTkwODE2\n" +
+        "MDUwMzI3WjB9MHswPTAJBgUrDgMCGgUABBT+SNWbr2JHc1Sa4gmqFP0g3Oa49AQU\n" +
+        "mtpA0z5braXCrNxIRzpWM1cBpJoCBAEAABahFhgPMjAxOTA4MTYwNDU5MzFaoAMK\n" +
+        "AQEYDzIwMTkwODE2MDUwMzI3WqARGA8yMDE5MDgxNzA1MDMyN1qhIzAhMB8GCSsG\n" +
+        "AQUFBzABAgQSBBBbPvAp5xnQ39vLywYMAWbPMA0GCSqGSIb3DQEBCwUAA4IBAQBX\n" +
+        "Ii5GX3Nu9Jqk5ARv+hXlYoJMia+cy02AcVcQiPE250dtNu7tfkX4FhtCDtN+HqqB\n" +
+        "xYUEBk95KPXZiLt7Dla9B38KC5i5gscGBPUW2tGa2wFyqXkG+blPasOc+O5DlvUU\n" +
+        "294HpH3QIVKnZioGpfIHR8h5sa0CGaXykEK4qyjw7IWD7mf5xQZflwE50Ez/0nDi\n" +
+        "NhN+MOp8kfHNfgQmzbx3dLL8LT1j5qdQ9cdkdXwn+DF6v6SJXwn/hOdFDUr4eZ7L\n" +
+        "RAzgaAKvL6DbOGWtnw7fifx++agTzQWkjAto4ekTkzyHK74mqBuyT/6vkgppcPuD\n" +
+        "osE9qhBxWJYQsqWNydXEoIIEdTCCBHEwggRtMIICVaADAgECAgIBADANBgkqhkiG\n" +
+        "9w0BAQsFADAlMRAwDgYDVQQKDAdUZXN0UEtJMREwDwYDVQQDDAhUZXN0Um9vdDAe\n" +
+        "Fw0xNTA1MDYwNjAwMDBaFw0yNTAxMjExMjAwMDBaMC0xEDAOBgNVBAoMB1Rlc3RQ\n" +
+        "S0kxGTAXBgNVBAMMEENBMSBJbnRlcm1lZGlhdGUwggEiMA0GCSqGSIb3DQEBAQUA\n" +
+        "A4IBDwAwggEKAoIBAQC10qMGl64kkS92LjAwYcTnLuKde4O8wKOSMVG/1c4fbQFU\n" +
+        "AgMxMx4gHKTo1Alh55pNEZdNrBk3z9xpe1C6qxEbP+ra9r8j4Dkci+276+6N1WSy\n" +
+        "7Ztpp8YzRCXwZ3OPwx0sZv6QYWOL8Vd/7Y8vxR+meZSKQs3vbyUX1vNwDTjojwHj\n" +
+        "dKuIUl5g6Us98T/KM2Dl5xx0w8MHDuEpA9Vtd+lyZZEmb+8aUJHfxF3tx5gXf9y1\n" +
+        "//QnwNpvur3whOkf2rk0UP6jY0lmgWrscE90ZMgnvebdY/MtGtT4k8PvGz3CCa65\n" +
+        "GrqFT+U9J3kglKQn9zJeSZrVHAE7guatTZpYMaE3AgMBAAGjgZ4wgZswHQYDVR0O\n" +
+        "BBYEFJraQNM+W62lwqzcSEc6VjNXAaSaMB8GA1UdIwQYMBaAFPBYgi5QQCd4Bcl6\n" +
+        "53eKhqSZODfbMA8GA1UdEwEB/wQFMAMBAf8wDgYDVR0PAQH/BAQDAgGGMDgGCCsG\n" +
+        "AQUFBwEBBCwwKjAoBggrBgEFBQcwAYYcaHR0cDovL2ppYW4ub3N0YXBsZS5vcmc6\n" +
+        "NzEwMDANBgkqhkiG9w0BAQsFAAOCAgEANGiCKcoWKAs4rPU0k3zrDrp4/evgP/lC\n" +
+        "rz4b0OJqyZOEtB9pVgG+5pH2QvcP1WvUQRdEaYLExMd61bRJD9Kwx5FsF/wiFyTg\n" +
+        "w54AL1wzKy+01+OnDFoCHrMXltfimOPfeAmmM8s0Bmu7T2tIXQYij0LE+H2sE6Sa\n" +
+        "22uc8NfustH33raL8/gofIeScYzuBjRTzx126CQA2IUiIB8FEYXPcCoyIyLdU/lJ\n" +
+        "MI/STRpv9+uR6/oL+qre2HiBD8GiD/lhqha/MYCztChDv6vQEhFmEbvVZAoPhjHV\n" +
+        "rjdGcbe0U8sXOeTSh16Hyyv8Ijcf/5t7LIkt3TYCm6NmgB8wfNNyr/buY7Vm8Dmp\n" +
+        "K3jCxvuCzCchjBdslkEFGiLEYUPtU/Q0RdESWOkS4YgjLaWQdvnA1Xc4QyYCagrD\n" +
+        "HUZdZdF9BEeqfV/b9ctgicODS7274bpRfd52fqvA/6+UhfYLHioL/JfjODtjExcL\n" +
+        "CuiX3X/vC6CRQY+OZTqqYr5O27Bk1AbbC0IHgUldrRhnIN3tT8UfudZm0b0GDOku\n" +
+        "K33SELj6blXgT/sjqyW/DGmQTvLQ2O62PL+py6KXqKas5H9qm7BPV5wCbKObVSak\n" +
+        "o8svNicToC1Io5Od0ZraI3I8Oux4UvLtNQX9+D929njo6ai5Jfe6kAVGe+ldhfBJ\n" +
+        "BFDZL7ASduk=";
+
+    // Saturday, August 17, 2019 2:00:00 AM GMT
+    static final Date VALID_DATE = new Date(1566007200000L);
+
+    public static void main(String[] args) throws Exception {
+        CertificateFactory certFac = CertificateFactory.getInstance("X.509");
+        CertPathValidator cpv = CertPathValidator.getInstance("PKIX");
+        X509Certificate goodGuyCert = getCert(certFac, GOOD_SERVER_PEM);
+        X509Certificate badGuyCert = getCert(certFac, BAD_SERVER_PEM);
+        X509Certificate intCACert = getCert(certFac, INT_CA_PEM);
+        X509Certificate rootCACert = getCert(certFac, ROOT_CA_PEM);
+        byte[] goodOcspDer = pemToDer(GOOD_GUY_OCSP_PEM);
+        byte[] badOcspDer = pemToDer(BAD_GUY_OCSP_PEM);
+        byte[] intCAOcspDer = pemToDer(INT_CA_OCSP_PEM);
+        Set<TrustAnchor> trustAnchors =
+                Set.of(new TrustAnchor(rootCACert, null));
+        PKIXRevocationChecker pkrc;
+        PKIXBuilderParameters pkixParams;
+
+        X509Certificate[] goodPath = { goodGuyCert, intCACert,
+            rootCACert };
+        X509Certificate[] badPath = { badGuyCert, intCACert,
+            rootCACert };
+
+        List<byte[]> goodResponses = List.of(goodOcspDer, intCAOcspDer);
+        List<byte[]> badResponses = List.of(badOcspDer, intCAOcspDer);
+
+        // Test 1: Path validation with revocation explicitly turned
+        // off, expected to pass.
+        pkixParams = new PKIXBuilderParameters(trustAnchors, null);
+        pkixParams.setDate(VALID_DATE);
+        pkixParams.setRevocationEnabled(false);
+        validatePath(goodPath, Collections.emptyList(), pkixParams, null);
+
+        // Test 2: Path validation with revocation turned on using the
+        // good path.  Should fail due to no responses.
+        pkixParams = new PKIXBuilderParameters(trustAnchors, null);
+        pkixParams.setDate(VALID_DATE);
+        validatePath(goodPath, Collections.emptyList(), pkixParams,
+            new CertPathValidatorException("KABOOM! No OCSP Responses!", null,
+            certFac.generateCertPath(List.of(goodGuyCert, intCACert)), 1,
+            BasicReason.UNDETERMINED_REVOCATION_STATUS));
+
+        // Test 3: Path validation of the good path with revocation turned
+        // on and available valid OCSP responses delivered through the
+        // List<byte[]> rather than via the PKIXRevocationChecker.
+        pkixParams = new PKIXBuilderParameters(trustAnchors, null);
+        pkixParams.setDate(VALID_DATE);
+        pkrc = (PKIXRevocationChecker)cpv.getRevocationChecker();
+        pkixParams.addCertPathChecker(pkrc);
+        validatePath(goodPath, goodResponses, pkixParams, null);
+
+        // Test 4: Path validation of the bad path with revocation explicitly
+        // disabled and valid OCSP responses provided through the PKIXRevocationChecker
+        // indicating a revoked certificate.  Even with the
+        // setRevocationEnabled method set to false this should perform
+        // revocation checking and catch the revoked certificate.
+        pkixParams = new PKIXBuilderParameters(trustAnchors, null);
+        pkixParams.setDate(
