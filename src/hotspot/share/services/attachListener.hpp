13a50acf62ee022126759391b8a1c8b43220f615
nevercum <121445512+nevercum@users.noreply.github.com>
2021-03-27 19:44:01 +0000

    Added a License

--- src/hotspot/share/services/attachListener.hpp
+++ src/hotspot/share/services/attachListener.hpp
@@ -70,4 +70,125 @@ class AttachListener: AllStatic {
   // check unix domain socket file on filesystem
   static bool check_socket_file() NOT_SERVICES_RETURN_(false);
 
-  // indicates if the 
+  // indicates if the Attach Listener needs to be created at startup
+  static bool init_at_startup() NOT_SERVICES_RETURN_(false);
+
+  // indicates if we have a trigger to start the Attach Listener
+  static bool is_init_trigger() NOT_SERVICES_RETURN_(false);
+
+#if !INCLUDE_SERVICES
+  static bool is_attach_supported()             { return false; }
+#else
+
+ private:
+  static volatile AttachListenerState _state;
+
+ public:
+  static void set_state(AttachListenerState new_state) {
+    Atomic::store(&_state, new_state);
+  }
+
+  static AttachListenerState get_state() {
+    return Atomic::load(&_state);
+  }
+
+  static AttachListenerState transit_state(AttachListenerState new_state,
+                                           AttachListenerState cmp_state) {
+    return Atomic::cmpxchg(&_state, cmp_state, new_state);
+  }
+
+  static bool is_initialized() {
+    return Atomic::load(&_state) == AL_INITIALIZED;
+  }
+
+  static void set_initialized() {
+    Atomic::store(&_state, AL_INITIALIZED);
+  }
+
+  // indicates if this VM supports attach-on-demand
+  static bool is_attach_supported()             { return !DisableAttachMechanism; }
+
+  // platform specific initialization
+  static int pd_init();
+
+  // platform specific operation
+  static AttachOperationFunctionInfo* pd_find_operation(const char* name);
+
+  // platform specific flag change
+  static jint pd_set_flag(AttachOperation* op, outputStream* out);
+
+  // platform specific detachall
+  static void pd_detachall();
+
+  // platform specific data dump
+  static void pd_data_dump();
+
+  // dequeue the next operation
+  static AttachOperation* dequeue();
+#endif // !INCLUDE_SERVICES
+
+ private:
+  static bool has_init_error(TRAPS);
+};
+
+#if INCLUDE_SERVICES
+class AttachOperation: public CHeapObj<mtInternal> {
+ public:
+  enum {
+    name_length_max = 16,       // maximum length of  name
+    arg_length_max = 1024,      // maximum length of argument
+    arg_count_max = 3           // maximum number of arguments
+  };
+
+  // name of special operation that can be enqueued when all
+  // clients detach
+  static char* detachall_operation_name() { return (char*)"detachall"; }
+
+ private:
+  char _name[name_length_max+1];
+  char _arg[arg_count_max][arg_length_max+1];
+
+ public:
+  const char* name() const                      { return _name; }
+
+  // set the operation name
+  void set_name(const char* name) {
+    assert(strlen(name) <= name_length_max, "exceeds maximum name length");
+    size_t len = MIN2(strlen(name), (size_t)name_length_max);
+    memcpy(_name, name, len);
+    _name[len] = '\0';
+  }
+
+  // get an argument value
+  const char* arg(int i) const {
+    assert(i>=0 && i<arg_count_max, "invalid argument index");
+    return _arg[i];
+  }
+
+  // set an argument value
+  void set_arg(int i, char* arg) {
+    assert(i>=0 && i<arg_count_max, "invalid argument index");
+    if (arg == nullptr) {
+      _arg[i][0] = '\0';
+    } else {
+      assert(strlen(arg) <= arg_length_max, "exceeds maximum argument length");
+      size_t len = MIN2(strlen(arg), (size_t)arg_length_max);
+      memcpy(_arg[i], arg, len);
+      _arg[i][len] = '\0';
+    }
+  }
+
+  // create an operation of a given name
+  AttachOperation(const char* name) {
+    set_name(name);
+    for (int i=0; i<arg_count_max; i++) {
+      set_arg(i, nullptr);
+    }
+  }
+
+  // complete operation by sending result code and any result data to the client
+  virtual void complete(jint result, bufferedStream* result_stream) = 0;
+};
+#endif // INCLUDE_SERVICES
+
+#endif // SHARE_SERVICES_ATTACHLISTENER_HPP
