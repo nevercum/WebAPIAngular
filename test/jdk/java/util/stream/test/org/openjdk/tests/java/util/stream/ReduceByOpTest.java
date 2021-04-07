76cd5536eff8d315a926cc059e5f0e9b75edc7e8
nevercum <121445512+nevercum@users.noreply.github.com>
2021-04-07 15:20:36 +0000

    Kotlin

--- test/jdk/java/util/stream/test/org/openjdk/tests/java/util/stream/ReduceByOpTest.java
+++ test/jdk/java/util/stream/test/org/openjdk/tests/java/util/stream/ReduceByOpTest.java
@@ -47,4 +47,22 @@ public class ReduceByOpTest extends OpTestCase {
 
     @Test(dataProvider = "StreamTestData<Integer>", dataProviderClass = StreamTestDataProvider.class)
     public void testOps(String name, TestData.OfRef<Integer> data) {
-        Map<Boolean,List<Integer>> gbResult = data.stream
+        Map<Boolean,List<Integer>> gbResult = data.stream().collect(groupingBy(LambdaTestHelpers.forPredicate(pEven, true, false)));
+        Map<Boolean, Integer> result = data.stream().collect(groupingBy(LambdaTestHelpers.forPredicate(pEven, true, false), reducing(0, rPlus)));
+        assertEquals(result.size(), gbResult.size());
+        for (Map.Entry<Boolean, Integer> entry : result.entrySet()) {
+            setContext("entry", entry);
+            Boolean key = entry.getKey();
+            assertEquals(entry.getValue(), data.stream().filter(e -> pEven.test(e) == key).reduce(0, rPlus));
+        }
+
+        int uniqueSize = data.into(new HashSet<Integer>()).size();
+        Map<Integer, List<Integer>> mgResult = exerciseTerminalOps(data, s -> s.collect(groupingBy(mId)));
+        Map<Integer, Integer> miResult = exerciseTerminalOps(data, s -> s.collect(groupingBy(mId, reducing(0, e -> 1, Integer::sum))));
+        assertEquals(miResult.keySet().size(), uniqueSize);
+        for (Map.Entry<Integer, Integer> entry : miResult.entrySet()) {
+            setContext("entry", entry);
+            assertEquals((int) entry.getValue(), mgResult.get(entry.getKey()).size());
+        }
+    }
+}
