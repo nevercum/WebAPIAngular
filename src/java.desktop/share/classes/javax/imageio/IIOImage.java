cf3552e1f80151b72091ba51d025481807c78d74
nevercum <121445512+nevercum@users.noreply.github.com>
2021-04-24 11:55:19 +0000

    Some cleanup.

--- src/java.desktop/share/classes/javax/imageio/IIOImage.java
+++ src/java.desktop/share/classes/javax/imageio/IIOImage.java
@@ -222,4 +222,101 @@ public class IIOImage {
         synchronized(this) {
             if (raster == null) {
                 throw new IllegalArgumentException("raster == null!");
- 
+            }
+            this.raster = raster;
+            this.image = null;
+        }
+    }
+
+    /**
+     * Returns the number of thumbnails stored in this
+     * {@code IIOImage}.
+     *
+     * @return the number of thumbnails, as an {@code int}.
+     */
+    public int getNumThumbnails() {
+        return thumbnails == null ? 0 : thumbnails.size();
+    }
+
+    /**
+     * Returns a thumbnail associated with the main image.
+     *
+     * @param index the index of the desired thumbnail image.
+     *
+     * @return a thumbnail image, as a {@code BufferedImage}.
+     *
+     * @throws IndexOutOfBoundsException if the supplied index is
+     * negative or larger than the largest valid index.
+     * @throws ClassCastException if a
+     * non-{@code BufferedImage} object is encountered in the
+     * list of thumbnails at the given index.
+     *
+     * @see #getThumbnails
+     * @see #setThumbnails
+     */
+    public BufferedImage getThumbnail(int index) {
+        if (thumbnails == null) {
+            throw new IndexOutOfBoundsException("No thumbnails available!");
+        }
+        return (BufferedImage)thumbnails.get(index);
+    }
+
+    /**
+     * Returns the current {@code List} of thumbnail
+     * {@code BufferedImage}s, or {@code null} if none is
+     * set.  A live reference is returned.
+     *
+     * @return the current {@code List} of
+     * {@code BufferedImage} thumbnails, or {@code null}.
+     *
+     * @see #getThumbnail(int)
+     * @see #setThumbnails
+     */
+    public List<? extends BufferedImage> getThumbnails() {
+        return thumbnails;
+    }
+
+    /**
+     * Sets the list of thumbnails to a new {@code List} of
+     * {@code BufferedImage}s, or to {@code null}.  The
+     * reference to the previous {@code List} is discarded.
+     *
+     * <p> The {@code thumbnails} argument must either be
+     * {@code null} or contain only {@code BufferedImage}
+     * objects.
+     *
+     * @param thumbnails a {@code List} of
+     * {@code BufferedImage} thumbnails, or {@code null}.
+     *
+     * @see #getThumbnail(int)
+     * @see #getThumbnails
+     */
+    public void setThumbnails(List<? extends BufferedImage> thumbnails) {
+        this.thumbnails = thumbnails;
+    }
+
+    /**
+     * Returns a reference to the current {@code IIOMetadata}
+     * object, or {@code null} is none is set.
+     *
+     * @return an {@code IIOMetadata} object, or {@code null}.
+     *
+     * @see #setMetadata
+     */
+    public IIOMetadata getMetadata() {
+        return metadata;
+    }
+
+    /**
+     * Sets the {@code IIOMetadata} to a new object, or
+     * {@code null}.
+     *
+     * @param metadata an {@code IIOMetadata} object, or
+     * {@code null}.
+     *
+     * @see #getMetadata
+     */
+    public void setMetadata(IIOMetadata metadata) {
+        this.metadata = metadata;
+    }
+}
