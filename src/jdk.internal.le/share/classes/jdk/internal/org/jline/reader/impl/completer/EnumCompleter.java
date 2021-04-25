c06816e0f676a84432588356f3100a53af219865
nevercum <121445512+nevercum@users.noreply.github.com>
2021-04-25 17:15:42 +0000

    edited title

--- src/jdk.internal.le/share/classes/jdk/internal/org/jline/reader/impl/completer/EnumCompleter.java
+++ src/jdk.internal.le/share/classes/jdk/internal/org/jline/reader/impl/completer/EnumCompleter.java
@@ -19,4 +19,12 @@ import jdk.internal.org.jline.reader.Completer;
  * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
  * @since 2.3
  */
-public class EnumCompleter extends StringsCompl
+public class EnumCompleter extends StringsCompleter
+{
+    public EnumCompleter(Class<? extends Enum<?>> source) {
+        Objects.requireNonNull(source);
+        for (Enum<?> n : source.getEnumConstants()) {
+            candidates.add(new Candidate(n.name().toLowerCase()));
+        }
+    }
+}
