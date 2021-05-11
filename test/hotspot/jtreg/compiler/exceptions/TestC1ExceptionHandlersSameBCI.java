a717321bf4a62a1621bbfb2382941bc186834522
nevercum <121445512+nevercum@users.noreply.github.com>
2021-05-11 19:09:03 +0000

    Updated changelog.

--- test/hotspot/jtreg/compiler/exceptions/TestC1ExceptionHandlersSameBCI.java
+++ test/hotspot/jtreg/compiler/exceptions/TestC1ExceptionHandlersSameBCI.java
@@ -37,4 +37,39 @@ public class TestC1ExceptionHandlersSameBCI {
 
     }
 
-    static void not_in
+    static void not_inline1() throws Ex1, Ex2 {
+
+    }
+
+    static void not_inline2(int v) {
+
+    }
+
+    static void test1() throws Ex1, Ex2 {
+        int i = 0;
+        try {
+            not_inline1();
+            i = 1;
+            not_inline1();
+        } catch (Ex1|Ex2 ex) {
+            not_inline2(i);
+        }
+    }
+
+    static void test2() {
+        int i = 0;
+        try {
+            test1();
+            i = 1;
+            test1();
+        } catch (Ex1|Ex2 ex) {
+            not_inline2(i);
+        }
+    }
+
+    static public void main(String[] args) {
+        for (int i = 0; i < 5000; i++) {
+            test2();
+        }
+    }
+}
