8a5d01a3590c5b626973740d7aa53d91633c75fd
nevercum <121445512+nevercum@users.noreply.github.com>
2021-06-05 11:55:35 +0000

    Update doc 22.0

--- test/jdk/java/awt/Modal/ToFront/FrameToFrontAppModal5Test.java
+++ test/jdk/java/awt/Modal/ToFront/FrameToFrontAppModal5Test.java
@@ -18,4 +18,33 @@
  *
  * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
  * or visit www.oracle.com if you need additional information or have any
- * q
+ * questions.
+ */
+
+import java.awt.Dialog;
+
+/*
+ * @test
+ * @key headful
+ * @bug 8050885
+ * @summary Check that calling toFront method for a frame in presence of
+ *          blocking application modal dialog having a visible Frame parent
+ *          does not bring the frame to the top of the modal dialog.
+ *
+ * @library ../helpers /lib/client/
+ * @library /test/lib
+ * @build ExtendedRobot
+ * @build Flag
+ * @build TestDialog
+ * @build TestFrame
+ * @run main FrameToFrontAppModal5Test
+ */
+
+public class FrameToFrontAppModal5Test {
+
+    public static void main(String[] args) throws Exception {
+        (new FrameToFrontModalBlockedTest(
+             Dialog.ModalityType.APPLICATION_MODAL,
+             FrameToFrontModalBlockedTest.DialogOwner.FRAME)).doTest();
+    }
+}
