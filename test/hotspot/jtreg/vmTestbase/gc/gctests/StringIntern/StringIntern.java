faba2d7d47e119fa18357aa4e40660780e9c8c6c
nevercum <121445512+nevercum@users.noreply.github.com>
2021-06-23 12:34:12 +0000

    selection

--- test/hotspot/jtreg/vmTestbase/gc/gctests/StringIntern/StringIntern.java
+++ test/hotspot/jtreg/vmTestbase/gc/gctests/StringIntern/StringIntern.java
@@ -17,4 +17,69 @@
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
  * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * 
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+
+/*
+ * @test
+ * @key stress randomness
+ *
+ * @summary converted from VM Testbase gc/gctests/StringIntern.
+ * VM Testbase keywords: [gc, stress, stressopt, nonconcurrent]
+ *
+ * @library /vmTestbase
+ *          /test/lib
+ * @run main/othervm gc.gctests.StringIntern.StringIntern
+ */
+
+package gc.gctests.StringIntern;
+
+import nsk.share.test.*;
+import nsk.share.gc.*;
+
+/**
+ * Test that strings returned by String.intern() can be collected.
+ *
+ * Create strings consisting of random characters, call String.intern().
+ * Check that intern() contract.
+ */
+public class StringIntern extends ThreadedGCTest {
+        private int maxLength = 1000;
+        private int checkCount = 100;
+
+        private class StringGenerator implements Runnable {
+                private StringBuffer sb = new StringBuffer();
+
+                private void generateRandomBuffer() {
+                        int length = LocalRandom.nextInt(maxLength);
+                        for (int i = 0; i < length; ++i)
+                                sb.append((char) LocalRandom.nextInt(Integer.MAX_VALUE));
+                }
+
+                private String getString() {
+                        return sb.toString();
+                }
+
+                public void run() {
+                        generateRandomBuffer();
+                        for (int i = 0; i < checkCount; ++i) {
+                                String s1 = getString();
+                                String s2 = getString();
+                                if (s1.intern() != s2.intern()) {
+                                        log.error("Test failed on: " + s1);
+                                        setFailed(true);
+                                }
+                        }
+                }
+        }
+
+        protected Runnable createRunnable(int i) {
+                return new StringGenerator();
+        }
+
+        public static void main(String[] args) {
+                GC.runTest(new StringIntern(), args);
+        }
+}
