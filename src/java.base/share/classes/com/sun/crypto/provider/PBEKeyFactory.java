1f5303255a03b94295b134a58bccc3d8fd7eb4b6
nevercum <121445512+nevercum@users.noreply.github.com>
2021-08-27 07:17:09 +0000

    [Modify] pod spec

--- src/java.base/share/classes/com/sun/crypto/provider/PBEKeyFactory.java
+++ src/java.base/share/classes/com/sun/crypto/provider/PBEKeyFactory.java
@@ -128,4 +128,206 @@ abstract class PBEKeyFactory extends SecretKeyFactorySpi {
 
     public static final class PBEWithHmacSHA1AndAES_128 extends PBEKeyFactory {
         public PBEWithHmacSHA1AndAES_128() {
-            super("PBEWi
+            super("PBEWithHmacSHA1AndAES_128");
+        }
+    }
+
+    public static final class PBEWithHmacSHA224AndAES_128
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA224AndAES_128() {
+            super("PBEWithHmacSHA224AndAES_128");
+        }
+    }
+
+    public static final class PBEWithHmacSHA256AndAES_128
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA256AndAES_128() {
+            super("PBEWithHmacSHA256AndAES_128");
+        }
+    }
+
+    public static final class PBEWithHmacSHA384AndAES_128
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA384AndAES_128() {
+            super("PBEWithHmacSHA384AndAES_128");
+        }
+    }
+
+    public static final class PBEWithHmacSHA512AndAES_128
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA512AndAES_128() {
+            super("PBEWithHmacSHA512AndAES_128");
+        }
+    }
+
+    public static final class PBEWithHmacSHA512_224AndAES_128
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA512_224AndAES_128() {
+            super("PBEWithHmacSHA512/224AndAES_128");
+        }
+    }
+
+    public static final class PBEWithHmacSHA512_256AndAES_128
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA512_256AndAES_128() {
+            super("PBEWithHmacSHA512/256AndAES_128");
+        }
+    }
+
+    public static final class PBEWithHmacSHA1AndAES_256 extends PBEKeyFactory {
+        public PBEWithHmacSHA1AndAES_256() {
+            super("PBEWithHmacSHA1AndAES_256");
+        }
+    }
+
+    public static final class PBEWithHmacSHA224AndAES_256
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA224AndAES_256() {
+            super("PBEWithHmacSHA224AndAES_256");
+        }
+    }
+
+    public static final class PBEWithHmacSHA256AndAES_256
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA256AndAES_256() {
+            super("PBEWithHmacSHA256AndAES_256");
+        }
+    }
+
+    public static final class PBEWithHmacSHA384AndAES_256
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA384AndAES_256() {
+            super("PBEWithHmacSHA384AndAES_256");
+        }
+    }
+
+    public static final class PBEWithHmacSHA512AndAES_256
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA512AndAES_256() {
+            super("PBEWithHmacSHA512AndAES_256");
+        }
+    }
+
+    public static final class PBEWithHmacSHA512_224AndAES_256
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA512_224AndAES_256() {
+            super("PBEWithHmacSHA512/224AndAES_256");
+        }
+    }
+
+    public static final class PBEWithHmacSHA512_256AndAES_256
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA512_256AndAES_256() {
+            super("PBEWithHmacSHA512/256AndAES_256");
+        }
+    }
+
+    /**
+     * Generates a <code>SecretKey</code> object from the provided key
+     * specification (key material).
+     *
+     * @param keySpec the specification (key material) of the secret key
+     *
+     * @return the secret key
+     *
+     * @exception InvalidKeySpecException if the given key specification
+     * is inappropriate for this key factory to produce a public key.
+     */
+    protected SecretKey engineGenerateSecret(KeySpec keySpec)
+        throws InvalidKeySpecException
+    {
+        if (!(keySpec instanceof PBEKeySpec)) {
+            throw new InvalidKeySpecException("Invalid key spec");
+        }
+        return new PBEKey((PBEKeySpec)keySpec, type, true);
+    }
+
+    /**
+     * Returns a specification (key material) of the given key
+     * in the requested format.
+     *
+     * @param key the key
+     *
+     * @param keySpecCl the requested format in which the key material shall be
+     * returned
+     *
+     * @return the underlying key specification (key material) in the
+     * requested format
+     *
+     * @exception InvalidKeySpecException if the requested key specification is
+     * inappropriate for the given key, or the given key cannot be processed
+     * (e.g., the given key has an unrecognized algorithm or format).
+     */
+    protected KeySpec engineGetKeySpec(SecretKey key, Class<?> keySpecCl)
+        throws InvalidKeySpecException {
+        if ((key instanceof SecretKey)
+            && (validTypes.contains(key.getAlgorithm().toUpperCase(Locale.ENGLISH)))
+            && (key.getFormat().equalsIgnoreCase("RAW"))) {
+
+            // Check if requested key spec is amongst the valid ones
+            if ((keySpecCl != null)
+                    && keySpecCl.isAssignableFrom(PBEKeySpec.class)) {
+                byte[] passwdBytes = key.getEncoded();
+                char[] passwdChars = new char[passwdBytes.length];
+                for (int i=0; i<passwdChars.length; i++)
+                    passwdChars[i] = (char) (passwdBytes[i] & 0x7f);
+                PBEKeySpec ret = new PBEKeySpec(passwdChars);
+                // password char[] was cloned in PBEKeySpec constructor,
+                // so we can zero it out here
+                java.util.Arrays.fill(passwdChars, ' ');
+                java.util.Arrays.fill(passwdBytes, (byte)0x00);
+                return ret;
+            } else {
+                throw new InvalidKeySpecException("Invalid key spec");
+            }
+        } else {
+            throw new InvalidKeySpecException("Invalid key "
+                                              + "format/algorithm");
+        }
+    }
+
+    /**
+     * Translates a <code>SecretKey</code> object, whose provider may be
+     * unknown or potentially untrusted, into a corresponding
+     * <code>SecretKey</code> object of this key factory.
+     *
+     * @param key the key whose provider is unknown or untrusted
+     *
+     * @return the translated key
+     *
+     * @exception InvalidKeyException if the given key cannot be processed by
+     * this key factory.
+     */
+    protected SecretKey engineTranslateKey(SecretKey key)
+        throws InvalidKeyException
+    {
+        try {
+            if ((key != null) &&
+                (validTypes.contains(key.getAlgorithm().toUpperCase(Locale.ENGLISH))) &&
+                (key.getFormat().equalsIgnoreCase("RAW"))) {
+
+                // Check if key originates from this factory
+                if (key instanceof com.sun.crypto.provider.PBEKey) {
+                    return key;
+                }
+
+                // Convert key to spec
+                PBEKeySpec pbeKeySpec = (PBEKeySpec)engineGetKeySpec
+                    (key, PBEKeySpec.class);
+
+                try {
+                    // Create key from spec, and return it
+                    return engineGenerateSecret(pbeKeySpec);
+                } finally {
+                    pbeKeySpec.clearPassword();
+                }
+            } else {
+                throw new InvalidKeyException("Invalid key format/algorithm");
+            }
+
+        } catch (InvalidKeySpecException ikse) {
+            throw new InvalidKeyException("Cannot translate key: "
+                                          + ikse.getMessage());
+        }
+    }
+}
