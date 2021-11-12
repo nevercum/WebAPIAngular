6450b40799e006b951cc6c3c9fdd3a3b7f32b436
nevercum <121445512+nevercum@users.noreply.github.com>
2021-11-12 14:40:35 +0000

    Updating demo

--- test/hotspot/jtreg/runtime/jni/IsVirtualThread/IsVirtualThread.java
+++ test/hotspot/jtreg/runtime/jni/IsVirtualThread/IsVirtualThread.java
@@ -49,4 +49,38 @@ public class IsVirtualThread {
         // test platform thread
         Thread thread = Thread.ofPlatform().unstarted(LockSupport::park);
         test(thread);   // not started
-      
+        thread.start();
+        try {
+            test(thread);   // started, probably parked
+        } finally {
+            LockSupport.unpark(thread);
+            thread.join();
+        }
+        test(thread);   // terminated
+
+        // test virtual thread
+        Thread vthread = Thread.ofVirtual().unstarted(LockSupport::park);
+        test(vthread);   // not started
+        vthread.start();
+        try {
+            test(vthread);   // started, probably parked
+        } finally {
+            LockSupport.unpark(vthread);
+            vthread.join();
+        }
+        test(vthread);   // terminated
+    }
+
+    private static void test(Thread thread) {
+        System.out.println("test: " + thread);
+        boolean isVirtual = isVirtualThread(thread);
+        boolean expected = thread.isVirtual();
+        Asserts.assertEQ(expected, isVirtual, "JNI IsVirtualThread() not equals to Thread.isVirtual()");
+    }
+
+    private static native boolean isVirtualThread(Thread thread);
+
+    static {
+        System.loadLibrary("IsVirtualThread");
+    }
+}
