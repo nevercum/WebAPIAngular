2ac717b09ee872fbe1f9025c3c68ebdfefe54c60
nevercum <121445512+nevercum@users.noreply.github.com>
2021-11-29 11:46:58 +0000

    something

--- test/jdk/javax/net/ssl/sanity/interop/CipherTest.java
+++ test/jdk/javax/net/ssl/sanity/interop/CipherTest.java
@@ -201,4 +201,236 @@ public class CipherTest {
             }
         } catch (InterruptedException e) {
             setFailed();
-            e.printStack
+            e.printStackTrace();
+        }
+        if (failed) {
+            throw new Exception("*** Test '" + peerFactory.getName() +
+                "' failed ***");
+        } else {
+            System.out.println("Test '" + peerFactory.getName() +
+                "' completed successfully");
+        }
+    }
+
+    synchronized TestParameters getTest() {
+        if (failed) {
+            return null;
+        }
+        if (testIterator.hasNext()) {
+            return (TestParameters)testIterator.next();
+        }
+        return null;
+    }
+
+    SSLSocketFactory getFactory() {
+        return factory;
+    }
+
+    static abstract class Client implements Runnable {
+
+        final CipherTest cipherTest;
+
+        Client(CipherTest cipherTest) throws Exception {
+            this.cipherTest = cipherTest;
+        }
+
+        public final void run() {
+            while (true) {
+                TestParameters params = cipherTest.getTest();
+                if (params == null) {
+                    // no more tests
+                    break;
+                }
+                if (!params.isEnabled()) {
+                    System.out.println("Skipping disabled test " + params);
+                    continue;
+                }
+                try {
+                    runTest(params);
+                    System.out.println("Passed " + params);
+                } catch (Exception e) {
+                    cipherTest.setFailed();
+                    System.out.println("** Failed " + params + "**");
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        abstract void runTest(TestParameters params) throws Exception;
+
+        void sendRequest(InputStream in, OutputStream out) throws IOException {
+            out.write("GET / HTTP/1.0\r\n\r\n".getBytes());
+            out.flush();
+            StringBuilder sb = new StringBuilder();
+            while (true) {
+                int ch = in.read();
+                if (ch < 0) {
+                    break;
+                }
+                sb.append((char)ch);
+            }
+            String response = sb.toString();
+            if (response.startsWith("HTTP/1.0 200 ") == false) {
+                throw new IOException("Invalid response: " + response);
+            }
+        }
+
+    }
+
+    // for some reason, ${test.src} has a different value when the
+    // test is called from the script and when it is called directly...
+    static String pathToStores = "../../etc";
+    static String pathToStoresSH = ".";
+    static String keyStoreFile = "keystore";
+    static String trustStoreFile = "truststore";
+    static char[] passwd = "passphrase".toCharArray();
+
+    static File PATH;
+
+    private static KeyStore readKeyStore(String name) throws Exception {
+        File file = new File(PATH, name);
+        InputStream in = new FileInputStream(file);
+        KeyStore ks = KeyStore.getInstance("JKS");
+        ks.load(in, passwd);
+        in.close();
+        return ks;
+    }
+
+    public static void main(PeerFactory peerFactory, String[] args)
+            throws Exception {
+        long time = System.currentTimeMillis();
+        String relPath;
+        if ((args != null) && (args.length > 0) && args[0].equals("sh")) {
+            relPath = pathToStoresSH;
+        } else {
+            relPath = pathToStores;
+        }
+        PATH = new File(System.getProperty("test.src", "."), relPath);
+        CipherTest.peerFactory = peerFactory;
+        System.out.println(
+            "Initializing test '" + peerFactory.getName() + "'...");
+        secureRandom = new SecureRandom();
+        secureRandom.nextInt();
+        trustStore = readKeyStore(trustStoreFile);
+        keyStore = readKeyStore(keyStoreFile);
+        KeyManagerFactory keyFactory =
+            KeyManagerFactory.getInstance(
+                KeyManagerFactory.getDefaultAlgorithm());
+        keyFactory.init(keyStore, passwd);
+        keyManager = (X509ExtendedKeyManager)keyFactory.getKeyManagers()[0];
+        trustManager = new AlwaysTrustManager();
+
+        CipherTest cipherTest = new CipherTest(peerFactory);
+        Thread serverThread = new Thread(peerFactory.newServer(cipherTest),
+            "Server");
+        serverThread.setDaemon(true);
+        serverThread.start();
+        System.out.println("Done");
+        cipherTest.run();
+        time = System.currentTimeMillis() - time;
+        System.out.println("Done. (" + time + " ms)");
+    }
+
+    static abstract class PeerFactory {
+
+        abstract String getName();
+
+        abstract Client newClient(CipherTest cipherTest) throws Exception;
+
+        abstract Server newServer(CipherTest cipherTest) throws Exception;
+
+        boolean isSupported(String cipherSuite, String protocol) {
+            // ignore exportable cipher suite for TLSv1.1
+            if (protocol.equals("TLSv1.1")
+                    && (cipherSuite.indexOf("_EXPORT_WITH") != -1)) {
+                    System.out.println("Skipping obsoleted test for " +
+                                        cipherSuite + " of " + protocol);
+                    return false;
+            }
+
+            // ignore obsoleted cipher suite for the specified protocol
+            // TODO
+
+            // ignore unsupported cipher suite for the specified protocol
+            // TODO
+
+            return true;
+        }
+    }
+
+}
+
+// we currently don't do any chain verification. we assume that works ok
+// and we can speed up the test. we could also just add a plain certificate
+// chain comparision with our trusted certificates.
+class AlwaysTrustManager implements X509TrustManager {
+
+    public AlwaysTrustManager() {
+
+    }
+
+    public void checkClientTrusted(X509Certificate[] chain, String authType)
+            throws CertificateException {
+        // empty
+    }
+
+    public void checkServerTrusted(X509Certificate[] chain, String authType)
+            throws CertificateException {
+        // empty
+    }
+
+    public X509Certificate[] getAcceptedIssuers() {
+        return new X509Certificate[0];
+    }
+}
+
+class MyX509KeyManager extends X509ExtendedKeyManager {
+
+    private final X509ExtendedKeyManager keyManager;
+    private String authType;
+
+    MyX509KeyManager(X509ExtendedKeyManager keyManager) {
+        this.keyManager = keyManager;
+    }
+
+    void setAuthType(String authType) {
+        this.authType = authType;
+    }
+
+    public String[] getClientAliases(String keyType, Principal[] issuers) {
+        if (authType == null) {
+            return null;
+        }
+        return keyManager.getClientAliases(authType, issuers);
+    }
+
+    public String chooseClientAlias(String[] keyType, Principal[] issuers,
+            Socket socket) {
+        if (authType == null) {
+            return null;
+        }
+        return keyManager.chooseClientAlias(new String[] {authType},
+            issuers, socket);
+    }
+
+    public String chooseEngineClientAlias(String[] keyType,
+            Principal[] issuers, SSLEngine engine) {
+        if (authType == null) {
+            return null;
+        }
+        return keyManager.chooseEngineClientAlias(new String[] {authType},
+            issuers, engine);
+    }
+
+    public String[] getServerAliases(String keyType, Principal[] issuers) {
+        throw new UnsupportedOperationException("Servers not supported");
+    }
+
+    public String chooseServerAlias(String keyType, Principal[] issuers,
+            Socket socket) {
+        throw new UnsupportedOperationException("Servers not supported");
+    }
+
+    public String chooseEngineServerAlias(String keyType, Principal[] issuers,
+            SSLEngine engine) {
+        throw new UnsupportedOperationExc
