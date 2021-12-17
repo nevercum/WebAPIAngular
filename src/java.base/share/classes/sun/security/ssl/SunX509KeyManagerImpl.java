d9906c5acd368e0a65b5dda081b6facc2685d91b
nevercum <121445512+nevercum@users.noreply.github.com>
2021-12-17 06:38:06 +0000

    gui stuff

--- src/java.base/share/classes/sun/security/ssl/SunX509KeyManagerImpl.java
+++ src/java.base/share/classes/sun/security/ssl/SunX509KeyManagerImpl.java
@@ -106,4 +106,233 @@ final class SunX509KeyManagerImpl extends X509ExtendedKeyManager {
         final X509Certificate[] certificates;
         private final Set<X500Principal> issuerX500Principals;
 
-        X509Credentials(PrivateKey pri
+        X509Credentials(PrivateKey privateKey, X509Certificate[] certificates) {
+            // assert privateKey and certificates != null
+            this.privateKey = privateKey;
+            this.certificates = certificates;
+            this.issuerX500Principals = HashSet.newHashSet(certificates.length);
+            for (X509Certificate certificate : certificates) {
+                issuerX500Principals.add(certificate.getIssuerX500Principal());
+            }
+        }
+
+        Set<X500Principal> getIssuerX500Principals() {
+            return issuerX500Principals;
+        }
+    }
+
+    SunX509KeyManagerImpl(KeyStore ks, char[] password)
+            throws KeyStoreException,
+            NoSuchAlgorithmException, UnrecoverableKeyException {
+
+        credentialsMap = new HashMap<>();
+        serverAliasCache = Collections.synchronizedMap(
+                new HashMap<>());
+        if (ks == null) {
+            return;
+        }
+
+        for (Enumeration<String> aliases = ks.aliases();
+                                        aliases.hasMoreElements(); ) {
+            String alias = aliases.nextElement();
+            if (!ks.isKeyEntry(alias)) {
+                continue;
+            }
+            Key key = ks.getKey(alias, password);
+            if (!(key instanceof PrivateKey)) {
+                continue;
+            }
+            Certificate[] certs = ks.getCertificateChain(alias);
+            if ((certs == null) || (certs.length == 0) ||
+                    !(certs[0] instanceof X509Certificate)) {
+                continue;
+            }
+            if (!(certs instanceof X509Certificate[])) {
+                Certificate[] tmp = new X509Certificate[certs.length];
+                System.arraycopy(certs, 0, tmp, 0, certs.length);
+                certs = tmp;
+            }
+
+            X509Credentials cred = new X509Credentials((PrivateKey)key,
+                (X509Certificate[])certs);
+            credentialsMap.put(alias, cred);
+            if (SSLLogger.isOn && SSLLogger.isOn("keymanager")) {
+                SSLLogger.fine("found key for : " + alias, (Object[])certs);
+            }
+        }
+    }
+
+    /*
+     * Returns the certificate chain associated with the given alias.
+     *
+     * @return the certificate chain (ordered with the user's certificate first
+     * and the root certificate authority last)
+     */
+    @Override
+    public X509Certificate[] getCertificateChain(String alias) {
+        if (alias == null) {
+            return null;
+        }
+        X509Credentials cred = credentialsMap.get(alias);
+        if (cred == null) {
+            return null;
+        } else {
+            return cred.certificates.clone();
+        }
+    }
+
+    /*
+     * Returns the key associated with the given alias
+     */
+    @Override
+    public PrivateKey getPrivateKey(String alias) {
+        if (alias == null) {
+            return null;
+        }
+        X509Credentials cred = credentialsMap.get(alias);
+        if (cred == null) {
+            return null;
+        } else {
+            return cred.privateKey;
+        }
+    }
+
+    /*
+     * Choose an alias to authenticate the client side of a secure
+     * socket given the public key type and the list of
+     * certificate issuer authorities recognized by the peer (if any).
+     */
+    @Override
+    public String chooseClientAlias(String[] keyTypes, Principal[] issuers,
+            Socket socket) {
+        /*
+         * We currently don't do anything with socket, but
+         * someday we might.  It might be a useful hint for
+         * selecting one of the aliases we get back from
+         * getClientAliases().
+         */
+
+        if (keyTypes == null) {
+            return null;
+        }
+
+        for (int i = 0; i < keyTypes.length; i++) {
+            String[] aliases = getClientAliases(keyTypes[i], issuers);
+            if ((aliases != null) && (aliases.length > 0)) {
+                return aliases[0];
+            }
+        }
+        return null;
+    }
+
+    /*
+     * Choose an alias to authenticate the client side of an
+     * <code>SSLEngine</code> connection given the public key type
+     * and the list of certificate issuer authorities recognized by
+     * the peer (if any).
+     *
+     * @since 1.5
+     */
+    @Override
+    public String chooseEngineClientAlias(String[] keyType,
+            Principal[] issuers, SSLEngine engine) {
+        /*
+         * If we ever start using socket as a selection criteria,
+         * we'll need to adjust this.
+         */
+        return chooseClientAlias(keyType, issuers, null);
+    }
+
+    /*
+     * Choose an alias to authenticate the server side of a secure
+     * socket given the public key type and the list of
+     * certificate issuer authorities recognized by the peer (if any).
+     */
+    @Override
+    public String chooseServerAlias(String keyType,
+            Principal[] issuers, Socket socket) {
+        /*
+         * We currently don't do anything with socket, but
+         * someday we might.  It might be a useful hint for
+         * selecting one of the aliases we get back from
+         * getServerAliases().
+         */
+        if (keyType == null) {
+            return null;
+        }
+
+        String[] aliases;
+
+        if (issuers == null || issuers.length == 0) {
+            aliases = serverAliasCache.get(keyType);
+            if (aliases == null) {
+                aliases = getServerAliases(keyType, issuers);
+                // Cache the result (positive and negative lookups)
+                if (aliases == null) {
+                    aliases = STRING0;
+                }
+                serverAliasCache.put(keyType, aliases);
+            }
+        } else {
+            aliases = getServerAliases(keyType, issuers);
+        }
+        if ((aliases != null) && (aliases.length > 0)) {
+            return aliases[0];
+        }
+        return null;
+    }
+
+    /*
+     * Choose an alias to authenticate the server side of an
+     * <code>SSLEngine</code> connection given the public key type
+     * and the list of certificate issuer authorities recognized by
+     * the peer (if any).
+     *
+     * @since 1.5
+     */
+    @Override
+    public String chooseEngineServerAlias(String keyType,
+            Principal[] issuers, SSLEngine engine) {
+        /*
+         * If we ever start using socket as a selection criteria,
+         * we'll need to adjust this.
+         */
+        return chooseServerAlias(keyType, issuers, null);
+    }
+
+    /*
+     * Get the matching aliases for authenticating the client side of a secure
+     * socket given the public key type and the list of
+     * certificate issuer authorities recognized by the peer (if any).
+     */
+    @Override
+    public String[] getClientAliases(String keyType, Principal[] issuers) {
+        return getAliases(keyType, issuers);
+    }
+
+    /*
+     * Get the matching aliases for authenticating the server side of a secure
+     * socket given the public key type and the list of
+     * certificate issuer authorities recognized by the peer (if any).
+     */
+    @Override
+    public String[] getServerAliases(String keyType, Principal[] issuers) {
+        return getAliases(keyType, issuers);
+    }
+
+    /*
+     * Get the matching aliases for authenticating the either side of a secure
+     * socket given the public key type and the list of
+     * certificate issuer authorities recognized by the peer (if any).
+     *
+     * Issuers come to us in the form of X500Principal[].
+     */
+    private String[] getAliases(String keyType, Principal[] issuers) {
+        if (keyType == null) {
+            return null;
+        }
+        if (issuers == null) {
+            issuers = new X500Principal[0];
+        }
+        if (!(issuers instanceof X500Principal[])) {
+            // normally, this will never happen but try to recover if it
