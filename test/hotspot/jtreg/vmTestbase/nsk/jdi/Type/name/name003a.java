be1432c4a9f7b4ae2c4d1c493940e55355be4479
nevercum <121445512+nevercum@users.noreply.github.com>
2022-01-21 15:05:33 +0000

    :memo: CHANGELOG

--- test/hotspot/jtreg/vmTestbase/nsk/jdi/Type/name/name003a.java
+++ test/hotspot/jtreg/vmTestbase/nsk/jdi/Type/name/name003a.java
@@ -40,4 +40,78 @@ public class name003a {
     static final int FAILED = 2;
     static final int PASS_BASE = 95;
 
-    //--------------------------------------------------   log pr
+    //--------------------------------------------------   log procedures
+
+    static boolean verbMode = false;  // debugger may switch to true
+
+    private static void log1(String message) {
+        if (verbMode)
+            System.err.println("**> name003a: " + message);
+    }
+
+    private static void logErr(String message) {
+        if (verbMode)
+            System.err.println("!!**> name003a: " + message);
+    }
+
+    //====================================================== test program
+
+    public void testMethod () {
+        return;
+    }
+
+    //----------------------------------------------------   main method
+
+    public static void main (String argv[]) {
+
+        for (int i=0; i<argv.length; i++) {
+            if ( argv[i].equals("-vbs") || argv[i].equals("-verbose") ) {
+                verbMode = true;
+                break;
+            }
+        }
+        log1("debuggee started!");
+
+        // informing a debugger of readyness
+        ArgumentHandler argHandler = new ArgumentHandler(argv);
+        IOPipe pipe = argHandler.createDebugeeIOPipe();
+        pipe.println("ready");
+
+
+        int exitCode = PASSED;
+        for (int i = 0; ; i++) {
+
+            String instruction;
+
+            log1("waiting for an instruction from the debugger ...");
+            instruction = pipe.readln();
+            if (instruction.equals("quit")) {
+                log1("'quit' recieved");
+                break ;
+
+            } else if (instruction.equals("newcheck")) {
+                switch (i) {
+
+    //------------------------------------------------------  section tested
+
+                case 0:
+                                pipe.println("checkready");
+                                break ;
+
+    //-------------------------------------------------    standard end section
+
+                default:
+                                pipe.println("checkend");
+                                break ;
+                }
+
+            } else {
+                logErr("ERRROR: unexpected instruction: " + instruction);
+                exitCode = FAILED;
+                break ;
+            }
+        }
+
+        System.exit(exitCode + PASS_BASE);
+    }
+}
