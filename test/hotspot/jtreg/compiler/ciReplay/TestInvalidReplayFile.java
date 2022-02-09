8d24fd2e5bc606eda61bf92cc43191990c1b417a
nevercum <121445512+nevercum@users.noreply.github.com>
2022-02-09 13:47:57 +0000

    remove namespace

--- test/hotspot/jtreg/compiler/ciReplay/TestInvalidReplayFile.java
+++ test/hotspot/jtreg/compiler/ciReplay/TestInvalidReplayFile.java
@@ -43,4 +43,27 @@ public class TestInvalidReplayFile {
 
     public static void main(String[] args) throws Exception {
 
-        // This test also serves as a very basic sanity test for
+        // This test also serves as a very basic sanity test for release VMs (accepting the replay options and
+        // attempting to read the replay file). Most of the tests in ciReplay use -XX:CICrashAt to induce artificial
+        // crashes into the compiler, and that option is not available for release VMs. Therefore we cannot generate
+        // replay files as a test in release builds.
+
+        File f = new File("bogus-replay-file.txt");
+        FileWriter w = new FileWriter(f);
+        w.write("Bogus 123");
+        w.flush();
+        w.close();
+
+        ProcessBuilder pb = ProcessTools.createJavaProcessBuilder(
+                "-XX:+UnlockDiagnosticVMOptions",
+                "-Xmx100M",
+                "-XX:+ReplayCompiles", "-XX:ReplayDataFile=./bogus-replay-file.txt");
+
+        OutputAnalyzer output_detail = new OutputAnalyzer(pb.start());
+        output_detail.shouldNotHaveExitValue(0);
+        output_detail.shouldContain("Error while parsing");
+
+    }
+}
+
+
