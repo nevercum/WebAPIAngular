a4dae6d863fccda364c532e570aac39b5371ada3
nevercum <121445512+nevercum@users.noreply.github.com>
2022-02-13 20:37:22 +0000

    remove log

--- src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/formats/html/PackageUseWriter.java
+++ src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/formats/html/PackageUseWriter.java
@@ -102,4 +102,136 @@ public class PackageUseWriter extends SubWriterHolderWriter {
                                 ClassUseMapper mapper, PackageElement pkgElement)
             throws DocFileIOException {
         DocPath filename = DocPaths.PACKAGE_USE;
-        PackageUseWriter pkgusegen = new PackageUseWriter
+        PackageUseWriter pkgusegen = new PackageUseWriter(configuration, mapper, filename, pkgElement);
+        pkgusegen.generatePackageUseFile();
+    }
+
+    /**
+     * Generate the package use list.
+     * @throws DocFileIOException if there is a problem generating the package use page
+     */
+    protected void generatePackageUseFile() throws DocFileIOException {
+        HtmlTree body = getBody();
+        Content mainContent = new ContentBuilder();
+        if (usingPackageToUsedClasses.isEmpty()) {
+            mainContent.add(contents.getContent("doclet.ClassUse_No.usage.of.0", getLocalizedPackageName(packageElement)));
+        } else {
+            addPackageUse(mainContent);
+        }
+        bodyContents.addMainContent(mainContent);
+        bodyContents.setFooter(getFooter());
+        body.add(bodyContents);
+        printHtmlDocument(null,
+                getDescription("use", packageElement),
+                body);
+    }
+
+    /**
+     * Add the package use information.
+     *
+     * @param content the content to which the package use information will be added
+     */
+    protected void addPackageUse(Content content) {
+        Content c = new ContentBuilder();
+        if (configuration.packages.size() > 1) {
+            addPackageList(c);
+        }
+        addClassList(c);
+        content.add(c);
+    }
+
+    /**
+     * Add the list of packages that use the given package.
+     *
+     * @param content the content to which the package list will be added
+     */
+    protected void addPackageList(Content content) {
+        Content caption = contents.getContent(
+                "doclet.ClassUse_Packages.that.use.0",
+                getPackageLink(packageElement, getLocalizedPackageName(packageElement)));
+        var table = new Table<Void>(HtmlStyle.summaryTable)
+                .setCaption(caption)
+                .setHeader(getPackageTableHeader())
+                .setColumnStyles(HtmlStyle.colFirst, HtmlStyle.colLast);
+        for (String pkgname: usingPackageToUsedClasses.keySet()) {
+            PackageElement pkg = utils.elementUtils.getPackageElement(pkgname);
+            Content packageLink = links.createLink(htmlIds.forPackage(pkg),
+                    getLocalizedPackageName(pkg));
+            Content summary = new ContentBuilder();
+            if (pkg != null && !pkg.isUnnamed()) {
+                addSummaryComment(pkg, summary);
+            } else {
+                summary.add(Entity.NO_BREAK_SPACE);
+            }
+            table.addRow(packageLink, summary);
+        }
+        content.add(table);
+    }
+
+    /**
+     * Add the list of classes that use the given package.
+     *
+     * @param content the content to which the class list will be added
+     */
+    protected void addClassList(Content content) {
+        TableHeader classTableHeader = new TableHeader(
+                contents.classLabel, contents.descriptionLabel);
+        var ul = HtmlTree.UL(HtmlStyle.blockList);
+        for (String packageName : usingPackageToUsedClasses.keySet()) {
+            PackageElement usingPackage = utils.elementUtils.getPackageElement(packageName);
+            var section = HtmlTree.SECTION(HtmlStyle.detail)
+                    .setId(htmlIds.forPackage(usingPackage));
+            Content caption = contents.getContent(
+                    "doclet.ClassUse_Classes.in.0.used.by.1",
+                    getPackageLink(packageElement, getLocalizedPackageName(packageElement)),
+                    getPackageLink(usingPackage, getLocalizedPackageName(usingPackage)));
+            var table = new Table<Void>(HtmlStyle.summaryTable)
+                    .setCaption(caption)
+                    .setHeader(classTableHeader)
+                    .setColumnStyles(HtmlStyle.colFirst, HtmlStyle.colLast);
+            for (TypeElement te : usingPackageToUsedClasses.get(packageName)) {
+                DocPath dp = pathString(te,
+                        DocPaths.CLASS_USE.resolve(docPaths.forName(te)));
+                Content stringContent = Text.of(utils.getSimpleName(te));
+                Content typeContent = links.createLink(dp.fragment(htmlIds.forPackage(usingPackage).name()),
+                        stringContent);
+                Content summary = new ContentBuilder();
+                addIndexComment(te, summary);
+
+                table.addRow(typeContent, summary);
+            }
+            section.add(table);
+            ul.add(HtmlTree.LI(section));
+        }
+        var li = HtmlTree.SECTION(HtmlStyle.packageUses, ul);
+        content.add(li);
+    }
+
+    /**
+     * {@return the package use HTML BODY element}
+     */
+    private HtmlTree getBody() {
+        String packageText = resources.getText("doclet.Package");
+        String name = packageElement.isUnnamed() ? "" : utils.getPackageName(packageElement);
+        String title = resources.getText("doclet.Window_ClassUse_Header", packageText, name);
+        HtmlTree body = getBody(getWindowTitle(title));
+        ContentBuilder headingContent = new ContentBuilder();
+        headingContent.add(contents.getContent("doclet.ClassUse_Title", packageText));
+        headingContent.add(new HtmlTree(TagName.BR));
+        headingContent.add(name);
+        var heading = HtmlTree.HEADING_TITLE(Headings.PAGE_TITLE_HEADING,
+                HtmlStyle.title, headingContent);
+        var div = HtmlTree.DIV(HtmlStyle.header, heading);
+        bodyContents.setHeader(getHeader(PageMode.USE, packageElement))
+                .addMainContent(div);
+        return body;
+    }
+
+    @Override
+    protected Navigation getNavBar(PageMode pageMode, Element element) {
+        Content linkContent = getModuleLink(utils.elementUtils.getModuleOf(element),
+                contents.moduleLabel);
+        return super.getNavBar(pageMode, element)
+                .setNavLinkModule(linkContent);
+    }
+}
