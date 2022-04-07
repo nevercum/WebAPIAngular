64c39b6ae7ca93abfef565e960ca3483fd8c63b5
nevercum <121445512+nevercum@users.noreply.github.com>
2022-04-07 19:53:38 +0000

    main.css

--- test/hotspot/jtreg/vmTestbase/nsk/jdi/ThreadReference/frame/frame001/TestDescription.java
+++ test/hotspot/jtreg/vmTestbase/nsk/jdi/ThreadReference/frame/frame001/TestDescription.java
@@ -34,3 +34,53 @@
  *     ThreadReference.
  *     The test checks up that a result of the method
  *     com.sun.jdi.ThreadReference.frame()
+ *     complies with its spec:
+ *     public StackFrame frame(int index)
+ *                  throws IncompatibleThreadStateException
+ *     Returns the StackFrame at the given index in the thread's current call stack.
+ *     Index 0 retrieves the current frame;
+ *     higher indices retrieve caller frames.
+ *     The thread must be suspended (normally through an interruption to the VM)
+ *     to get this information, and
+ *     it is only valid until the thread is resumed again.
+ *     Parameters: index - the desired frame
+ *     Returns: the requested StackFrame
+ *     Throws: IncompatibleThreadStateException -
+ *             if the thread is not suspended in the target VM
+ *             java.lang.IndexOutOfBoundsException -
+ *             if the index is greater than or equal to frameCount() or
+ *             is negative.
+ *             ObjectCollectedException -
+ *             if this object has been garbage collected.
+ *     when a ThreadReference object has not been garbage collected,
+ *     hence ObjectCollectedException is not expected to be thrown.
+ *     The test works as follows:
+ *     The debugger program - nsk.jdi.ThreadReference.frame.frame001;
+ *     the debuggee program - nsk.jdi.ThreadReference.frame.frame001a.
+ *     Using nsk.jdi.share classes,
+ *     the debugger gets the debuggee running on another JavaVM,
+ *     creates the object debuggee.VM,
+ *     establishes a pipe with the debuggee program, and then
+ *     send to the programm commands, to which the debuggee replies
+ *     via the pipe. Upon getting reply,
+ *     the debugger calls corresponding debuggee.VM methods to get
+ *     needed data and compares the data got to the data expected.
+ *     In case of error the test produces the return value 97 and
+ *     a corresponding error message(s).
+ *     Otherwise, the test is passed and produces
+ *     the return value 95 and no message.
+ * COMMENTS:
+ *
+ * @library /vmTestbase
+ *          /test/lib
+ * @build nsk.jdi.ThreadReference.frame.frame001
+ *        nsk.jdi.ThreadReference.frame.frame001a
+ * @run main/othervm
+ *      nsk.jdi.ThreadReference.frame.frame001
+ *      -verbose
+ *      -arch=${os.family}-${os.simpleArch}
+ *      -waittime=5
+ *      -debugee.vmkind=java
+ *      -transport.address=dynamic
+ *      -debugee.vmkeys="${test.vm.opts} ${test.java.opts}"
+ */
