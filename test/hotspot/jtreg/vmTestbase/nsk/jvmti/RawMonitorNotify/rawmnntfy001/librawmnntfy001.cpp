82842404b73c2134a6475344135f0d156195c717
nevercum <121445512+nevercum@users.noreply.github.com>
2022-04-07 21:38:56 +0000

    new solutions

--- test/hotspot/jtreg/vmTestbase/nsk/jvmti/RawMonitorNotify/rawmnntfy001/librawmnntfy001.cpp
+++ test/hotspot/jtreg/vmTestbase/nsk/jvmti/RawMonitorNotify/rawmnntfy001/librawmnntfy001.cpp
@@ -1,12 +1,10 @@
 /*
- * Copyright (c) 2007, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -23,23 +21,13 @@
  * questions.
  */
 
-package com.sun.media.sound;
-
-/**
- * This interface is used for oscillators.
- * See example in ModelDefaultOscillator which is a wavetable oscillator.
- *
- * @author Karl Helgason
- */
-public interface ModelOscillator {
-
-    int getChannels();
-
-    /**
-     * Attenuation is in cB.
-     * @return
-     */
-    float getAttenuation();
-
-    ModelOscillatorStream open(float samplerate);
-}
+#include "native_thread.cpp"
+#include "nsk_tools.cpp"
+#include "jni_tools.cpp"
+#include "jvmti_tools.cpp"
+#include "agent_tools.cpp"
+#include "jvmti_FollowRefObjects.cpp"
+#include "Injector.cpp"
+#include "JVMTITools.cpp"
+#include "agent_common.cpp"
+#include "rawmnntfy001.cpp"
