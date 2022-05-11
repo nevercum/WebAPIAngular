8fbc288cb38ef78ddc2234596b254c4dabbafb08
nevercum <121445512+nevercum@users.noreply.github.com>
2022-05-11 13:13:17 +0000

    Update coskunz-ad-tr.txt

---
+++ src/java.desktop/share/native/libharfbuzz/hb-ot-var-mvar-table.hh
@@ -0,0 +1,8 @@
+/*
+ * Copyright © 2017  Google, Inc.
+ *
+ *  This is part of HarfBuzz, a text shaping library.
+ *
+ * Permission is hereby granted, without written agreement and without
+ * license or royalty fees, to use, copy, modify, and distribute this
+ * software and its documentation for any purpose, provided
