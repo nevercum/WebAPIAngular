9cd8e978d9b56af8fd8893c5c002524eb70552fa
nevercum <121445512+nevercum@users.noreply.github.com>
2022-06-05 11:50:54 +0000

    i7core_edac: Fix PCI device reference count

--- test/micro/org/openjdk/bench/java/lang/invoke/MethodHandleConvertReturnVoid.java
+++ test/micro/org/openjdk/bench/java/lang/invoke/MethodHandleConvertReturnVoid.java
@@ -20,4 +20,83 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package org.openjdk.bench.java.lang
+package org.openjdk.bench.java.lang.invoke;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Benchmark assesses runtime argument conversion performance for MethodHandles.
+ * This particular test checks the return type conversion: int -> void.
+ */
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Thread)
+@Warmup(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 2, timeUnit = TimeUnit.SECONDS)
+@Fork(3)
+public class MethodHandleConvertReturnVoid {
+
+    /*
+     * Implementation notes:
+     *
+     * Baseline is invokeExact call, which presumably measures the performance without argument conversion.
+     *
+     * The test is subdivided into three subtests, gradually doing more work:
+     *   - 1_Convert: calls MH.asType to do actual conversion
+     *   - 2_MTConvert: instantiates MT, and then calls MH.asType to do actual conversion
+     *   - 3_Call: calls MH.invoke, requesting argument conversion
+     *
+     * Calling virtual method to guarantee side-effects on value, without trading off scalability.
+     * This test should be executed in non-shared mode.
+     */
+
+    public int value;
+
+    private MethodHandle mh;
+    private MethodType newType;
+
+    @Setup
+    public void setup() throws Throwable {
+        mh = MethodHandles.lookup().findVirtual(MethodHandleConvertReturnVoid.class, "target", MethodType.methodType(int.class));
+        newType = MethodType.methodType(void.class, MethodHandleConvertReturnVoid.class);
+    }
+
+    @Benchmark
+    public int baselineExact() throws Throwable {
+        return (int) mh.invokeExact(this);
+    }
+
+    @Benchmark
+    public MethodHandle test_1_Convert() throws Throwable {
+        return mh.asType(newType);
+    }
+
+    @Benchmark
+    public MethodHandle test_2_MTConvert() throws Throwable {
+        return mh.asType(MethodType.methodType(void.class, MethodHandleConvertReturnVoid.class));
+    }
+
+    @Benchmark
+    public void test_3_Call() throws Throwable {
+        mh.invoke(this);
+    }
+
+    public int target() {
+        return value++;
+    }
+
+}
