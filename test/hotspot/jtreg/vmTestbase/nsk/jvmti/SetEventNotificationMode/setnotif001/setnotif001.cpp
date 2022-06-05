83a40903f0c0350791fc32ce3c2ba23a16ee38fc
nevercum <121445512+nevercum@users.noreply.github.com>
2022-06-05 19:35:26 +0000

    chore(release): 4.1.0

--- test/hotspot/jtreg/vmTestbase/nsk/jvmti/SetEventNotificationMode/setnotif001/setnotif001.cpp
+++ test/hotspot/jtreg/vmTestbase/nsk/jvmti/SetEventNotificationMode/setnotif001/setnotif001.cpp
@@ -65,4 +65,244 @@ void mark(jvmtiEnv *jvmti_env, jvmtiEvent kind) {
     ev_scale[kind] = 1;
     err = jvmti_env->RawMonitorExit(access_lock);
     if (err != JVMTI_ERROR_NONE) {
-        printf("(R
+        printf("(RawMonitorExit) unexpected error: %s (%d)\n",
+               TranslateError(err), err);
+        result = STATUS_FAILED;
+    }
+}
+
+void disable(jvmtiEnv *jvmti_env, jvmtiEvent kind) {
+    jvmtiError err;
+
+    if (printdump == JNI_TRUE) {
+        printf(">>> disabling %s\n", TranslateEvent(kind));
+    }
+    err = jvmti_env->SetEventNotificationMode(
+        JVMTI_DISABLE, kind, NULL);
+    if (err != JVMTI_ERROR_NONE) {
+        printf("Fail to disable %s: %s (%d)\n",
+               TranslateEvent(kind), TranslateError(err), err);
+        result = STATUS_FAILED;
+    }
+}
+
+void enable(jvmtiEnv *jvmti_env, jvmtiEvent kind) {
+    jvmtiError err;
+
+    if (printdump == JNI_TRUE) {
+        printf(">>> enabling %s\n", TranslateEvent(kind));
+    }
+    err = jvmti_env->RawMonitorEnter(access_lock);
+    if (err != JVMTI_ERROR_NONE) {
+        printf("(RawMonitorEnter) unexpected error: %s (%d)\n",
+               TranslateError(err), err);
+        result = STATUS_FAILED;
+    }
+    err = jvmti_env->SetEventNotificationMode(
+        JVMTI_ENABLE, kind, NULL);
+    if (err != JVMTI_ERROR_NONE) {
+        printf("Fail to enable %s: %s (%d)\n",
+               TranslateEvent(kind), TranslateError(err), err);
+        result = STATUS_FAILED;
+    } else {
+        enbl_scale[kind] = 1;
+    }
+    err = jvmti_env->RawMonitorExit(access_lock);
+    if (err != JVMTI_ERROR_NONE) {
+        printf("(RawMonitorExit) unexpected error: %s (%d)\n",
+               TranslateError(err), err);
+        result = STATUS_FAILED;
+    }
+}
+
+void setWatches(jvmtiEnv *jvmti_env, JNIEnv *env, jclass cls) {
+    jvmtiError err;
+    jfieldID fid;
+    jmethodID mid;
+
+    mid = env->GetStaticMethodID(cls, "meth01", "(I)V");
+    if (mid == NULL) {
+      printf("(GetStaticMethodID) returns NULL");
+      result = STATUS_FAILED;
+      return;
+    }
+    err = jvmti->SetBreakpoint(mid, 0);
+    if (err == JVMTI_ERROR_NONE) {
+        enable(jvmti_env, JVMTI_EVENT_BREAKPOINT);
+    } else {
+        result = STATUS_FAILED;
+        printf("(SetBreakpoint) unexpected error: %s (%d)\n",
+               TranslateError(err), err);
+    }
+
+    fid = env->GetStaticFieldID(cls, "fld", "I");
+    if (caps.can_generate_field_access_events) {
+        err = jvmti->SetFieldAccessWatch(cls, fid);
+        if (err == JVMTI_ERROR_NONE) {
+            enable(jvmti_env, JVMTI_EVENT_FIELD_ACCESS);
+        } else {
+            result = STATUS_FAILED;
+            printf("(SetFieldAccessWatch) unexpected error: %s (%d)\n",
+                   TranslateError(err), err);
+        }
+    } else {
+        printf("Warning: SetFieldAccessWatch is not implemented\n");
+    }
+
+    if (caps.can_generate_field_modification_events) {
+        err = jvmti_env->SetFieldModificationWatch(cls, fid);
+        if (err == JVMTI_ERROR_NONE) {
+            enable(jvmti_env, JVMTI_EVENT_FIELD_MODIFICATION);
+        } else {
+            result = STATUS_FAILED;
+            printf("(SetFieldModificationWatch) unexpected error: %s (%d)\n",
+                   TranslateError(err), err);
+        }
+    } else {
+        printf("Warning: SetFieldModificationWatch is not implemented\n");
+    }
+}
+
+void JNICALL VMInit(jvmtiEnv *jvmti_env, JNIEnv *env, jthread thr) {
+    if (printdump == JNI_TRUE) {
+        printf(">>> VMInit\n");
+    }
+    enable(jvmti_env, JVMTI_EVENT_SINGLE_STEP);
+    enable(jvmti_env, JVMTI_EVENT_EXCEPTION);
+    enable(jvmti_env, JVMTI_EVENT_EXCEPTION_CATCH);
+    mark(jvmti_env, JVMTI_EVENT_VM_INIT);
+    flag = 1;
+}
+
+void JNICALL SingleStep(jvmtiEnv *jvmti_env, JNIEnv *env,
+        jthread thread, jmethodID method, jlocation location) {
+    if (flag) {
+        mark(jvmti_env, JVMTI_EVENT_SINGLE_STEP);
+        disable(jvmti_env, JVMTI_EVENT_SINGLE_STEP);
+    }
+}
+
+void JNICALL MethodEntry(jvmtiEnv *jvmti_env, JNIEnv *env,
+        jthread thr, jmethodID method) {
+    jvmtiError err;
+    jboolean isNative;
+
+    if (flag) {
+        mark(jvmti_env, JVMTI_EVENT_METHOD_ENTRY);
+        if (env->IsSameObject(notifyFramePopThread, thr)) {
+            err = jvmti_env->IsMethodNative(method, &isNative);
+            if (err != JVMTI_ERROR_NONE) {
+                result = STATUS_FAILED;
+                printf("(IsMethodNative) unexpected error: %s (%d)\n",
+                       TranslateError(err), err);
+            }
+            if (isNative == JNI_FALSE) {
+                err = jvmti_env->NotifyFramePop(thr, 0);
+                if (err == JVMTI_ERROR_NONE) {
+                    enable(jvmti_env, JVMTI_EVENT_FRAME_POP);
+                } else {
+                    result = STATUS_FAILED;
+                    printf("(NotifyFramePop) unexpected error: %s (%d)\n",
+                           TranslateError(err), err);
+                }
+            }
+            enable(jvmti_env, JVMTI_EVENT_CLASS_LOAD);
+            enable(jvmti_env, JVMTI_EVENT_CLASS_PREPARE);
+            disable(jvmti_env, JVMTI_EVENT_METHOD_ENTRY);
+        }
+    }
+}
+
+void JNICALL ExceptionCatch(jvmtiEnv *jvmti_env, JNIEnv *env, jthread thr,
+        jmethodID method, jlocation location, jobject exception) {
+    if (flag) {
+        mark(jvmti_env, JVMTI_EVENT_EXCEPTION_CATCH);
+    }
+}
+
+void JNICALL MethodExit(jvmtiEnv *jvmti_env, JNIEnv *env,
+        jthread thr, jmethodID method,
+        jboolean was_poped_by_exc, jvalue return_value) {
+    if (flag) {
+        mark(jvmti_env, JVMTI_EVENT_METHOD_EXIT);
+        disable(jvmti_env, JVMTI_EVENT_METHOD_EXIT);
+    }
+}
+
+void JNICALL ThreadStart(jvmtiEnv *jvmti_env, JNIEnv *env,
+        jthread thread) {
+    if (flag) {
+        mark(jvmti_env, JVMTI_EVENT_THREAD_START);
+    }
+}
+
+void JNICALL ThreadEnd(jvmtiEnv *jvmti_env, JNIEnv *env,
+        jthread thread) {
+    if (flag) {
+        mark(jvmti_env, JVMTI_EVENT_THREAD_END);
+    }
+}
+
+void JNICALL ClassLoad(jvmtiEnv *jvmti_env, JNIEnv *env,
+        jthread thread, jclass klass) {
+    if (flag) {
+        mark(jvmti_env, JVMTI_EVENT_CLASS_LOAD);
+    }
+}
+
+void JNICALL ClassPrepare(jvmtiEnv *jvmti_env, JNIEnv *env,
+        jthread thread, jclass klass) {
+    if (flag) {
+        mark(jvmti_env, JVMTI_EVENT_CLASS_PREPARE);
+    }
+}
+
+void JNICALL Exception(jvmtiEnv *jvmti_env, JNIEnv *env, jthread thr,
+        jmethodID method, jlocation location, jobject exception,
+        jmethodID catch_method, jlocation catch_location) {
+    if (flag) {
+        mark(jvmti_env, JVMTI_EVENT_EXCEPTION);
+    }
+}
+
+void JNICALL FieldAccess(jvmtiEnv *jvmti_env, JNIEnv *env, jthread thr,
+        jmethodID method, jlocation location, jclass field_klass, jobject obj, jfieldID field) {
+    if (flag) {
+        mark(jvmti_env, JVMTI_EVENT_FIELD_ACCESS);
+    }
+}
+
+void JNICALL FieldModification(jvmtiEnv *jvmti_env, JNIEnv *env,
+        jthread thr, jmethodID method, jlocation location, jclass field_klass, jobject obj,
+        jfieldID field, char sig, jvalue new_value) {
+    if (flag) {
+        mark(jvmti_env, JVMTI_EVENT_FIELD_MODIFICATION);
+    }
+}
+
+void JNICALL Breakpoint(jvmtiEnv *jvmti_env, JNIEnv *env,
+        jthread thread, jmethodID method, jlocation location) {
+    if (flag) {
+        mark(jvmti_env, JVMTI_EVENT_BREAKPOINT);
+    }
+}
+
+void JNICALL FramePop(jvmtiEnv *jvmti_env, JNIEnv *env,
+        jthread thread, jmethodID method, jboolean wasPopedByException) {
+    if (flag) {
+        mark(jvmti_env, JVMTI_EVENT_FRAME_POP);
+    }
+}
+
+#ifdef STATIC_BUILD
+JNIEXPORT jint JNICALL Agent_OnLoad_setnotif001(JavaVM *jvm, char *options, void *reserved) {
+    return Agent_Initialize(jvm, options, reserved);
+}
+JNIEXPORT jint JNICALL Agent_OnAttach_setnotif001(JavaVM *jvm, char *options, void *reserved) {
+    return Agent_Initialize(jvm, options, reserved);
+}
+JNIEXPORT jint JNI_OnLoad_setnotif001(JavaVM *jvm, char *options, void *reserved) {
+    return JNI_VERSION_1_8;
+}
+#endif
+jint  Agent_Initialize(JavaVM *jvm, char *optio
