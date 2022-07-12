ed2c43f8c46c5e0651c78247df8fcce03a75e904
nevercum <121445512+nevercum@users.noreply.github.com>
2022-07-12 19:42:15 +0000

    Update checkpoint

--- test/hotspot/jtreg/vmTestbase/nsk/jvmti/scenarios/general_functions/GF01/gf01t001/gf01t001.cpp
+++ test/hotspot/jtreg/vmTestbase/nsk/jvmti/scenarios/general_functions/GF01/gf01t001/gf01t001.cpp
@@ -59,4 +59,142 @@ static int findProp(char *prop) {
         if (strcmp(expected_props[i], prop) == 0) {
             NSK_DISPLAY1("CHECK PASSED: found highly recommended system property \"%s\" as expected\n",
                 expected_props[i]);
-            return 1; /* the property 
+            return 1; /* the property found */
+        }
+    }
+
+    NSK_DISPLAY1("\tsystem property \"%s\" not found among highly recommended ones\n",
+        prop);
+    return 0; /* the property not found */
+}
+
+static void checkProps(jvmtiEnv *jvmti_env, const char *stepMsg) {
+    jint count;
+    char **propKeys;
+    char *prop;
+    int i;
+    int foundProps = 0;
+
+    NSK_DISPLAY1("%s: Getting system property keys ...\n",
+        stepMsg);
+    if (!NSK_JVMTI_VERIFY(jvmti_env->GetSystemProperties(&count, &propKeys))) {
+        result = STATUS_FAILED;
+        return;
+    }
+    NSK_DISPLAY1("%d keys obtained\n",
+        count);
+
+    if (count < PROP_NUM) {
+        result = STATUS_FAILED;
+        NSK_COMPLAIN2("TEST FAILED: GetSystemProperties() returns %d system property keys\n\texpected at least %d",
+            count, PROP_NUM);
+    }
+
+    for (i=0; i< count; i++) {
+        NSK_DISPLAY2("%d) getting property for the key \"%s\":\n",
+            i+1, propKeys[i]);
+       if (!NSK_JVMTI_VERIFY(jvmti_env->GetSystemProperty((const char*) propKeys[i], &prop))) {
+           result = STATUS_FAILED;
+           return;
+        }
+        NSK_DISPLAY1("\tproperty=\"%s\"\n",
+            prop);
+
+        foundProps += findProp(propKeys[i]);
+
+        NSK_DISPLAY0("\tdeallocating system property\n");
+        if (!NSK_JVMTI_VERIFY(jvmti_env->Deallocate((unsigned char*) prop))) {
+            result = STATUS_FAILED;
+            return;
+        }
+
+        NSK_DISPLAY0("\tdeallocating the system property key\n\n");
+        if (!NSK_JVMTI_VERIFY(jvmti_env->Deallocate((unsigned char*) propKeys[i]))) {
+            result = STATUS_FAILED;
+            return;
+        }
+    }
+
+/*    NSK_DISPLAY0("Deallocating the property key array ...\n");
+    if (!NSK_JVMTI_VERIFY(jvmti_env->Deallocate((unsigned char*) &propKeys))) {
+        result = STATUS_FAILED;
+        return;
+    }*/
+
+    if (foundProps != PROP_NUM) {
+        result = STATUS_FAILED;
+        NSK_COMPLAIN2("TEST FAILED: only %d highly recommended system properties found\n\tinstead of %d as expected\n",
+            foundProps, PROP_NUM);
+    }
+}
+
+/** callback functions **/
+void JNICALL
+VMInit(jvmtiEnv *jvmti_env, JNIEnv *env, jthread thr) {
+    NSK_DISPLAY0("VMInit event received\n");
+
+    checkProps(jvmti_env, ">>> b) TEST CASE \"VMInit\"");
+}
+
+void JNICALL
+VMDeath(jvmtiEnv *jvmti_env, JNIEnv *env) {
+    NSK_DISPLAY0("VMDeath event received\n");
+
+    checkProps(jvmti_env, ">>> c) TEST CASE \"VMDeath\"");
+
+    if (result == STATUS_FAILED)
+        exit(STATUS_FAILED);
+}
+/************************/
+
+JNIEXPORT jint JNICALL
+Java_nsk_jvmti_scenarios_general_1functions_GF01_gf01t001_check(JNIEnv *env, jobject obj) {
+    return result;
+}
+
+#ifdef STATIC_BUILD
+JNIEXPORT jint JNICALL Agent_OnLoad_gf01t001(JavaVM *jvm, char *options, void *reserved) {
+    return Agent_Initialize(jvm, options, reserved);
+}
+JNIEXPORT jint JNICALL Agent_OnAttach_gf01t001(JavaVM *jvm, char *options, void *reserved) {
+    return Agent_Initialize(jvm, options, reserved);
+}
+JNIEXPORT jint JNI_OnLoad_gf01t001(JavaVM *jvm, char *options, void *reserved) {
+    return JNI_VERSION_1_8;
+}
+#endif
+jint Agent_Initialize(JavaVM *jvm, char *options, void *reserved) {
+    jvmtiEnv *jvmti;
+
+    /* init framework and parse options */
+    if (!NSK_VERIFY(nsk_jvmti_parseOptions(options)))
+        return JNI_ERR;
+
+    /* create JVMTI environment */
+    if (!NSK_VERIFY((jvmti =
+            nsk_jvmti_createJVMTIEnv(jvm, reserved)) != NULL))
+        return JNI_ERR;
+
+    NSK_DISPLAY0("setting event callbacks ...\n");
+
+    (void) memset(&callbacks, 0, sizeof(callbacks));
+    callbacks.VMInit = &VMInit;
+    callbacks.VMDeath = &VMDeath;
+    if (!NSK_JVMTI_VERIFY(jvmti->SetEventCallbacks(&callbacks, sizeof(callbacks))))
+        return JNI_ERR;
+
+    NSK_DISPLAY0("setting event callbacks done\nenabling events ...\n");
+
+    if (!NSK_JVMTI_VERIFY(jvmti->SetEventNotificationMode(JVMTI_ENABLE, JVMTI_EVENT_VM_DEATH, NULL)))
+        return JNI_ERR;
+    if (!NSK_JVMTI_VERIFY(jvmti->SetEventNotificationMode(JVMTI_ENABLE, JVMTI_EVENT_VM_INIT, NULL)))
+        return JNI_ERR;
+
+    NSK_DISPLAY0("enabling events done\n\n");
+
+    checkProps(jvmti, ">>> a) TEST CASE \"OnLoad\"");
+
+    return JNI_OK;
+}
+
+}
